5f557ac4097f0cdfe986e2e9c1343ff4690f730b
Steffen L. Norgren <ironix@trollop.org>
2009-03-02 11:16:14 +0000

    Corrected a couple bugs and formatting issues with the client.

--- networking/trunk/client-server/client.c
+++ networking/trunk/client-server/client.c
@@ -3,7 +3,7 @@
 
 void start_client(char * server, char * port) {
 	struct addrinfo hints, *serverinfo, *p;
-	char ass[INET6_ADDRSTRLEN];
+	char serverIP[INET6_ADDRSTRLEN];
 	char sendbuf[MAXLEN], recvbuf[MAXLEN];
 	int sockfd;
 	int ret;
@@ -20,35 +20,37 @@ void start_client(char * server, char * port) {
 	for (p = serverinfo; p !=NULL; p = p->ai_next ) {
 
 		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			fprintf(stdout, "Socket create fail.\n");
+			perror("client: socket");
 			continue;
 		}
 
-		if ((connect(sockfd, p->ai_addr, p->ai_addrlen))==-1) {
-			fprintf(stdout, "connection fail.\n");
+		if ((connect(sockfd, p->ai_addr, p->ai_addrlen)) == -1) {
+			close(sockfd);
+			perror("client: connect");
 			continue;
 		}
 		break;
 	}
 
 	if (p == NULL) {
-		perror("failed to connect.\n");
+		fprintf(stderr, "client: failed to connect\n");
 		exit(2);
 	}
 	
-	inet_ntop(p->ai_family,get_in_addr((struct sockaddr*)p->ai_addr),ass,sizeof(ass));
+	inet_ntop(p->ai_family,get_in_addr((struct sockaddr*)p->ai_addr),serverIP,sizeof(serverIP));
 
 	freeaddrinfo(serverinfo);
 
+	/* TODO: Add the ability to detect server disconnects */
 	while (fgets(sendbuf, MAXLEN, stdin)) {
-		if ((send(sockfd, sendbuf, strlen(sendbuf), 0))==-1) {
-			fprintf(stdout, "send failed\n");
+		if ((send(sockfd, sendbuf, strlen(sendbuf), 0)) == -1) {
+			perror("send");
 		}
 		
-		if ((recv(sockfd, recvbuf, MAXLEN, 0))==-1) {
-			fprintf(stdout, "recv failed\n");
+		if ((recv(sockfd, recvbuf, MAXLEN, 0)) == -1) {
+			perror("recv");
 		}
 		
-		printf("%s", recvbuf);
+		fprintf(stdout, "%s", recvbuf);
 	}
 }

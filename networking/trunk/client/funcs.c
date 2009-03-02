be32bec8009b09152ae702849b80bf24c1aa68bc
Steffen L. Norgren <ironix@trollop.org>
2009-03-02 09:24:39 +0000

    Combining client and server into a single program

--- networking/trunk/client/funcs.c
+++
@@ -1,9 +0,0 @@
-#include "client.h"
-
-void *get_in_addr(struct sockaddr *sa)
-{
-	if (sa->sa_family == AF_INET){
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}

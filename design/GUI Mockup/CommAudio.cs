7a9de44fe653d5b317c4ea7b4e46e549245bfd73
Steffen L. Norgren <ironix@trollop.org>
2009-03-11 22:00:11 +0000

    GUI has been updated

--- design/GUI Mockup/CommAudio.cs
+++ design/GUI Mockup/CommAudio.cs
@@ -14,6 +14,46 @@ namespace GUI_Mockup
         public CommAudio()
         {
             InitializeComponent();
+            download.Enabled = false;
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!serverToolStripMenuItem.Checked)
+            {
+                new Connect().Show();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void seToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (serverToolStripMenuItem.Checked)
+            {
+                serverToolStripMenuItem.Checked = false;
+            }
+            seToolStripMenuItem.Checked = true;
+            removeFile.Enabled = false;
+            addFile.Enabled = false;
+            groupBox1.Enabled = false;
+            download.Enabled = true;
+        }
+
+        private void serverToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (seToolStripMenuItem.Checked)
+            {
+                seToolStripMenuItem.Checked = false;
+            }
+            serverToolStripMenuItem.Checked = true;
+            removeFile.Enabled = true;
+            addFile.Enabled = true;
+            groupBox1.Enabled = true;
+            download.Enabled = false;
         }
     }
 }
--- design/GUI Mockup/Program.cs
+++ design/GUI Mockup/Program.cs
@@ -15,7 +15,7 @@ namespace GUI_Mockup
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CommAudio());
         }
     }
 }

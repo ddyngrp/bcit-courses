9d99cad28d16937a04ee4d36cdf4a540887e5b90
Matt Woodske <mwoodske1@my.bcit.ca>
2009-03-21 23:22:06 +0000

    added a header with macros.  added more server logic.

--- logic/trunk/J M Server/server.cpp
+++ logic/trunk/J M Server/server.cpp
@@ -1,96 +1,138 @@
-#define UMOVE 10
-#define GRID_EMPTY 0
-#define GRID_BOMB 1
-
+/* need to add alive bool to player class */
+/* also it occurs to me that graphics are going need to implement some sort
+   of gui with a start button or something on the server and a list of players
+   or sth dunno they can figure it out */
+/* these need to go somewhere global so that networking can use them too */
 unsigned char grid[15][15];
-Player pArray[8];
+Player player_array[8];
+unsigned char player_count = 0;
+bool game_running = false; /* this needs to be changed in main as we go
+			      from pre-game to game to pre-game etc */
 
-int main(void)
-{
-	// Init shizzle
-	
-	for( ; ; )
-	{
-		// call receive function
-	}
-}
+void move_player(const int, const int);
+void plant_bomb(const int);
+void player_quit(const int);
+void player_join(void);
 
-void move_player(int pIndex, const char direction)
+void start_fuse(const int, const int);
+
+/* this should be somewhere global so that everyone can use it */
+void make_packet(char, char, char);
+
+void
+move_player(const int index, const int direction)
 {
-	char packet = 0;
+char packet;
 	
 	switch(direction)
 	{
-	case 0:
-		if(!pArray[pIndex].collision(direction))
+	case MOVE_UP:
+		if(player_array[index].collision(direction) == false)
 			break;
 		
-		pArray[pIndex].y_ -= UMOVE;	
-			
-		packet += pArray[pIndex].playerid_;
-		packet << 2;
-		
-		// send packet to network function
+		player_array[index].y_ -= UNITS_PER_MOVE;	
+		packet = make_packet(TYPE_MOVE, player_array[index].id_, MOVE_UP);
+		// send packet to all clients
 		break;
 		
-	case 1:
-		if(!pArray[pIndex].collision(direction))
+	case MOVE_DOWN:
+		if(player_array[index].collision(direction) == false)
 			break;
 			
-		pArray[pIndex].y_ += UMOVE;
-		
-		packet += pArray[pIndex].playerid_;
-		packet << 2;
-		
-		// send packet to network function
+		player_array[index].y_ += UNITS_PER_MOVE;
+		packet = make_packet(TYPE_MOVE, player_array[index].id_, MOVE_DOWN);
+		// send packet to all clients
 		break;
 		
-	case 2:
-		if(!pArray[pIndex].collision(direction))
+	case MOVE_LEFT:
+		if(player_array[index].collision(direction) == false)
 			break;
 			
-		pArray[pIndex].x_ -= UMOVE;
-		
-		packet += pArray[pIndex].playerid_;
-		packet << 2;
-		
-		// send packet to network function
+		player_array[index].x_ -= UNITS_PER_MOVE;
+		packet = make_packet(TYPE_MOVE, player_array[index].id_, MOVE_LEFT);
+		// send packet to all clients
 		break;
 		
-	case 3:
-		if(!pArray[pIndex].collision(direction))
+	case MOVE_RIGHT:
+		if(player_array[index].collision(direction) == false)
 			break;
 			
-		pArray[pIndex].x_ += UMOVE;
-		
-		packet += pArray[pIndex].playerid_;
-		packet << 2;
-		
-		// send packet to network function
+		player_array[index].x_ += UNITS_PER_MOVE;
+		packet = make_packet(TYPE_MOVE, player_array[index].id_, MOVE_RIGHT);
+		// send packet to all clients
 		break;
 	}
 }
-
-void plant_bomb(int pIndex)
+void
+plant_bomb(const int index)
 {
-	char packet = 0;
-	char temp = 0;
+char packet;
+int x;
+int y;
 	
-	if(!pArray[pIndex].canPlant)
+	if(player_array[index].canPlant() == false)
 		return;
-		
-	grid[pArray[pIndex].x_][pArray[pIndex].y_] = GRID_BOMB;
-	
-	pArray[pIndex].availableBombs--;
-	
-	start_fuse(pArray[pIndex].x_, pArray[pIndex].y_);
-	
-	temp = 2;
-	packet += temp << 5;
-	
-	temp = pArray[pIndex].playerid_;
-	packet += temp << 2;
-	
-	// send packet to network
+	x = player_array[index].x_;
+	y = player_array[index].y_;
+
+	grid[x][y] = GRID_BOMB;
+	player_array[index].droppedBombs_--;
+	start_fuse(x,y);
+	packet = make_packet(TYPE_PLANT, player_array[index].id_, 0);
+	// send packet to all clients
 }
 
+void
+start_fuse(const int x, const int y)
+{
+	return;	
+}
+
+void
+add_player(void)
+{
+char packet;
+char packet2;
+
+	if (player_count == MAX_PLAYERS)
+		return;
+	packet2 = make_packet(TYPE_ADDYOU, player_count, 0);
+	// send packet to _ONLY_ the player who joined
+	packet = make_packet(TYPE_JOIN, player_count, 0);
+	// send packet to all clients	
+	player_count++;
+	init_player(player_count);
+}
+
+void
+player_quit(const int id)
+{
+char packet;
+    
+	packet = make_packet(TYPE_QUIT, id, 0);
+	// send packet to all clients
+	player_array[id].alive = false;
+	player_count--;
+}
+
+char
+make_packet(char type, char id, char options)
+{
+char packet = 0;
+
+	packet += type >> 5;
+	packet += id >> 2;
+	packet += options;
+	return packet;
+}
+/* i guess we could add players as the game is running if we want to, we
+   would have to add some logic so they dont spawn on a block or whatever */
+void
+init_player(const int id)
+{
+	if (game_running == false)		
+		return;
+	player_array[id].x_ = rand() * 15;
+	player_array[id].y_ = rand() * 15;
+	player_array[id].alive = true;
+}

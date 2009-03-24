5cdc8ac76930e89873d0dfa7a132ae44b804edd5
David Young <dyoung117@my.bcit.ca>
2009-03-24 21:14:08 +0000

    s3rv3r shit yedIggggGggGGggG

--- logic/trunk/Server/defs.h
+++ logic/trunk/Server/defs.h
@@ -1,32 +1,39 @@
 #ifndef DEFS_H
 
-#define GRID_EMPTY       1
-#define GRID_BOMB        2
-#define GRID_DBLOCK      4
-#define GRID_UBLOCK      8
-#define GRID_POWUP_NUM   16
-#define GRID_POWUP_RAD   32
-#define GRID_POWUP_SPIKE 64
-#define GRID_FIRE        128
+#define GRID_EMPTY       	1
+#define GRID_BOMB        	2
+#define GRID_DBLOCK      	4
+#define GRID_UBLOCK      	8
+#define GRID_POWUP_NUM   	16
+#define GRID_POWUP_RAD   	32
+#define GRID_POWUP_SPIKE 	64
+#define GRID_FIRE        	128
 
-#define TYPE_MOVE        0
-#define TYPE_PLANT       1
-#define TYPE_EXPLODE     2
-#define TYPE_DIE         3
-#define TYPE_POWERUP     4
+#define TYPE_MOVE       	 0
+#define TYPE_PLANT       	1
+#define TYPE_EXPLODE     	2
+#define TYPE_DIE         	3
+#define TYPE_POWERUP     	4
 
-#define MOVE_UP          0
-#define MOVE_DOWN        1
-#define MOVE_LEFT        2
-#define MOVE_RIGHT       3
+#define MOVE_UP          	0
+#define MOVE_DOWN        	1
+#define MOVE_LEFT        	2
+#define MOVE_RIGHT       	3
 
-#define POWUP_NUMBER     0
-#define POWUP_RADIUS     1
-#define POWUP_SPIKE      2
+#define CS_UP          		0
+#define CS_DOWN        		1
+#define CS_LEFT        		2
+#define CS_RIGHT       		3
+#define CS_BOMB        		4
+#define CS_QUIT        		5
 
-#define UNITS_PER_MOVE   10
+#define POWUP_NUMBER     	0
+#define POWUP_RADIUS     	1
+#define POWUP_SPIKE      	2
 
-#define MAX_PLAYERS      8
+#define UNITS_PER_MOVE   	10
+
+#define MAX_PLAYERS      	8
 
 #define DEFS_H
 #endif

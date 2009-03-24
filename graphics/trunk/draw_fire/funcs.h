fdce2bba20c65e84f022356b9302754483b52611
Colby Last <Colbylast@shaw.ca>
2009-03-24 07:25:52 +0000

    Updated the code to the newest version.  This one supports transparency as well as modularized code.  Also I took the liberty of starting on the client side shell.

---
+++ graphics/trunk/draw_fire/funcs.h
@@ -0,0 +1,41 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include <string>
+
+#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 800
+#define TILE 35
+
+#define MAP_HEIGHT 11
+#define MAP_WIDTH 17
+
+#define ERROR -1
+
+typedef struct _Point{
+  int x,y;
+} Point, *pPoint;
+
+typedef struct _fire
+{
+  Point center;
+  int up, down, left, right;
+} fire, *pFire;
+
+SDL_Surface *load_image(std::string);
+void apply_surface(int, int, SDL_Surface*);
+
+void sdl_init();
+void game_loop(SDL_Surface *, SDL_Surface *, SDL_Surface *, int [MAP_HEIGHT][MAP_WIDTH]);
+
+bool draw_fire(fire f);
+void draw_fire_center(Point center);
+void draw_fire_arm_down(Point, int);
+void draw_fire_arm_up(Point, int);
+void draw_fire_arm_right(Point, int);
+void draw_fire_arm_left(Point, int);
+
+#endif
+
--- graphics/trunk/draw_fire/main.cpp
+++ graphics/trunk/draw_fire/main.cpp
@@ -1,45 +1,24 @@
+#include "funcs.h"
 
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
-
-#define SCREEN_HEIGHT 600
-#define SCREEN_WIDTH 800
-#define TILE 35
+using std::string;
 
 SDL_Surface *screen;
 
-typedef struct _Point{
-  int x,y;
-} Point, *pPoint;
-
-typedef struct _fire
-{
-  Point center;
-  int up, down, left, right;
-} fire, *pFire;
-
-SDL_Surface *load_image(char []);
-void display_image(SDL_Surface *image, SDL_Rect* src, SDL_Rect* dest, 
-				SDL_Surface *screen, int x, int y);
-void apply_surface(int, int, SDL_Surface*);
-bool draw_fire(fire f);
-
 int main( void ) 
 { 
-	int x, y;
 	fire fire_position;
-	int map[11][17] = {//5,5
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,1,1,2,2,1,2,2,2,1,2,2,2,2,1,1,0},
-	{0,1,0,2,0,2,0,1,0,1,0,1,0,2,0,1,0},
-	{0,1,2,2,1,1,1,2,2,2,2,2,2,2,1,2,0},
-	{0,2,0,2,0,1,0,2,0,1,0,2,0,2,0,2,0},
-	{0,2,2,1,1,0,1,1,2,2,1,2,2,2,2,1,0},
-	{0,1,0,2,0,1,0,2,0,2,0,1,0,2,0,2,0},
-	{0,2,2,1,2,1,2,2,2,2,2,2,1,2,2,1,0},
-	{0,1,0,2,0,2,0,1,0,2,0,2,0,1,0,1,0},
-	{0,1,1,1,2,2,1,2,2,2,2,2,2,1,1,1,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	int map[MAP_HEIGHT][MAP_WIDTH] = {
+	  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	  {0,1,1,2,2,1,2,2,2,1,2,2,2,2,1,1,0},
+	  {0,1,0,2,0,2,0,1,0,1,0,1,0,2,0,1,0},
+	  {0,1,2,2,1,1,1,2,2,2,2,2,2,2,1,2,0},
+	  {0,2,0,2,0,1,0,2,0,1,0,2,0,2,0,2,0},
+	  {0,2,2,1,1,0,1,1,2,2,1,2,2,2,2,1,0},
+	  {0,1,0,2,0,1,0,2,0,2,0,1,0,2,0,2,0},
+	  {0,2,2,1,2,1,2,2,2,2,2,2,1,2,2,1,0},
+	  {0,1,0,2,0,2,0,1,0,2,0,2,0,1,0,1,0},
+	  {0,1,1,1,2,2,1,2,2,2,2,2,2,1,1,1,0},
+	  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	};
 
 	fire_position.center.x = 5;
@@ -47,141 +26,87 @@ int main( void )
 
 	fire_position.up = 2;
 	fire_position.down = 3;
-	fire_position.left = 0;
-	fire_position.right = 7;
+	fire_position.left = 2;
+	fire_position.right = 4;
 
   //Start SDL
-  /*
-   * SDL_Surface *screen;
-   * 
-   */
-  /*--------------------------------------------------------*/
-  SDL_Init( SDL_INIT_EVERYTHING );
-
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {//turn video on
-    printf("Unable to initialize SDL: %s\n", SDL_GetError());
-    return 1;
-  }
- 
-  atexit(SDL_Quit);
- 
-  screen = SDL_SetVideoMode(SCREEN_HEIGHT, SCREEN_WIDTH, 16, SDL_DOUBLEBUF | SDL_FULLSCREEN);//video settings
-  if (screen == NULL) {
-    printf("Unable to set video mode: %s\n", SDL_GetError());
-    return 1;
-  }
+  sdl_init();
   /*--------------------------------------------------------*/
 
 
   SDL_Surface *imgWalkable;
   SDL_Surface *imgBreakable;
   SDL_Surface *imgIndestructable;
-  SDL_Surface *temp;
-  char grass[] = "grass.bmp";
-  char indes[] = "indestructable.bmp";
-  char destruct[] = "breakable.bmp";
 	
-  imgWalkable = load_image(grass);	
-  imgIndestructable = load_image(indes);
-  imgBreakable = load_image(destruct);
-  
-  SDL_Rect src, dest;
- 
-  src.x = 0;
-  src.y = 0;
-  src.w = TILE;
-  src.h = TILE;
+  imgWalkable = load_image("../images/map/grass.bmp");	
+  imgIndestructable = load_image("../images/map/indestructable.bmp");
+  imgBreakable = load_image("../images/map/breakable.bmp");
 
-  dest.w = TILE;
-  dest.h = TILE;
-  for(x = 0; (x/TILE) < 17; x += TILE){
-    for(y = 0; (y/TILE) < 11; y += TILE){
-      dest.x = x;
-      dest.y = y;
-      if(map[(y/TILE)][(x/TILE)] == 0){
-        SDL_BlitSurface(imgIndestructable, &src, screen, &dest);
-      }else if(map[(y/TILE)][(x/TILE)] == 1){
-        SDL_BlitSurface(imgWalkable, &src, screen, &dest);
-      }else{
-        SDL_BlitSurface(imgBreakable, &src, screen, &dest);
-      }
-    }
-  }
+  game_loop(imgWalkable, imgIndestructable, imgBreakable, map);
+
   draw_fire(fire_position);
   SDL_Flip(screen);
   SDL_Delay(5000);
-  SDL_Quit(); 
   return 0; 
 } 
 
-bool draw_fire(fire f){
-  SDL_Surface *imgCenter;
-  SDL_Surface *imgVertical;
-  SDL_Surface *imgHorizontal;
-  SDL_Surface *imgLeftEnd;
-  SDL_Surface *imgRightEnd;
-  SDL_Surface *imgTopEnd;
-  SDL_Surface *imgBottomEnd;
-  SDL_Surface *temp;
- 
-  imgCenter 	= load_image("center.xcf");
-  imgVertical	= load_image("vertical.xcf");
-  imgHorizontal = load_image("horizontal.xcf");
-  imgLeftEnd	= load_image("leftEnd.xcf");
-  imgRightEnd	= load_image("rightEnd.xcf");
-  imgTopEnd	= load_image("topEnd.xcf");
-  imgBottomEnd	= load_image("bottomEnd.xcf");
-  
-  apply_surface((f.center.x*TILE), (f.center.y*TILE), imgCenter);
-  
-  if(f.left != 0){
-    apply_surface((f.center.x - f.left) * TILE, (f.center.y) * TILE, imgLeftEnd);
-    f.left--;
+void game_loop(SDL_Surface *imgWalkable, SDL_Surface *imgIndestructable, SDL_Surface *imgBreakable, int map[MAP_HEIGHT][MAP_WIDTH]){
+	int x, y;
+  for(x = 0; (x/TILE) < MAP_WIDTH; x += TILE){
+    for(y = 0; (y/TILE) < MAP_HEIGHT; y += TILE){
+      switch(map[(y/TILE)][(x/TILE)]){
+      case 0:
+        apply_surface(x, y, imgIndestructable);
+        break;
+      case 1:
+        apply_surface(x, y, imgWalkable);
+        break;
+      case 2:
+        apply_surface(x, y, imgBreakable);
+        break;
+      default:
+        apply_surface(x, y, imgWalkable);
+        break;
+      }
+    }
   }
-  for(int position = (f.center.x - f.left); position != f.center.x; position++)
-    apply_surface((position * TILE), f.center.y * TILE, imgHorizontal);
-  	
-  if(f.right != 0){
-    apply_surface((f.center.x + f.right) * TILE, (f.center.y) * TILE, imgRightEnd);
-    f.right--;
-  }
-  for(int position = (f.center.x + f.right); position != f.center.x; position--)
-    apply_surface((position * TILE), f.center.y * TILE, imgHorizontal);
-  
-  if(f.up != 0){
-    apply_surface(f.center.x * TILE, (f.center.y - f.up) * TILE, imgTopEnd);
-    f.up--;
-  }
-  for(int position = (f.center.y - f.up); position != f.center.y; position++)
-    apply_surface(f.center.x * TILE, (position * TILE), imgVertical);
-  	
-  if(f.down != 0){
-    apply_surface(f.center.x * TILE, (f.center.y + f.down) * TILE, imgBottomEnd);
-    f.down--;
-  }
-  for(int position = (f.center.y + f.down); position != f.center.y; position--)
-  	apply_surface(f.center.x * TILE, (position * TILE), imgVertical);
-  return true;
 }
 
+void sdl_init(){
+  SDL_Init( SDL_INIT_EVERYTHING );
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {//turn video on
+    printf("Unable to initialize SDL: %s\n", SDL_GetError());
+    exit(1);
+  }
+  atexit(SDL_Quit);
+  screen = SDL_SetVideoMode(SCREEN_HEIGHT, SCREEN_WIDTH, 16, SDL_DOUBLEBUF);//video settings
+  if (screen == NULL) {
+    printf("Unable to set video mode: %s\n", SDL_GetError());
+    exit(2);
+  }
+  SDL_WM_SetCaption( "Tux Bomber", NULL ); 
+}
+
+
 //loads the images
-SDL_Surface *load_image(string filename)
-{
-  SDL_Surface* loadedImage = NULL; 
+SDL_Surface *load_image(string filename){
   SDL_Surface* optimizedImage = NULL; 
+  SDL_Surface* loadedImage = NULL; 
   
   loadedImage = IMG_Load(filename.c_str()); 
-  
   if( loadedImage != NULL ) { 
     optimizedImage = SDL_DisplayFormat( loadedImage ); 
     SDL_FreeSurface( loadedImage ); 
-  } 
+  }
+  if( optimizedImage != NULL ) { 
+    Uint32 colorkey = SDL_MapRGB( optimizedImage->format, 0, 0xFF, 0xFF ); 
+    SDL_SetColorKey( optimizedImage, SDL_SRCCOLORKEY, colorkey ); 
+  }
   return optimizedImage;
 }
 
 //shows images on screen
-void apply_surface( int x, int y, SDL_Surface* source)
-{
+void apply_surface( int x, int y, SDL_Surface* source){
   //Holds offsets
   SDL_Rect offset;
   SDL_Rect src;

bed18fb802c9a2d450d1bae1bcfb256f2da1f9cf
Alin Albu <aalbu@my.bcit.ca>
2009-03-27 18:27:38 +0000

    Stupid svn ^&%^&@

--- graphics/trunk/menu_pre_alpha_1/OkCancel.cpp
+++ graphics/trunk/menu_pre_alpha_1/OkCancel.cpp
@@ -102,28 +102,18 @@ int OkCancel::start(SDL_Event event)
 				if((event.motion.x > 90) && (event.motion.x < 140) &&
 						(event.motion.y > 470) && (event.motion.y < 510))
 				{
-<<<<<<< .mine
-					if(loaded_ == num_options_ - 2)
-						continue;
-=======
 					if (loaded_ == num_options_ - 2)
 						continue;
->>>>>>> .r133
+
 					loaded_ = num_options_ - 2;
 					Mix_PlayChannel(-1, music_, 0);
 				}else
 					if((event.motion.x > 400) && (event.motion.x < 550) &&
 							(event.motion.y > 470) && (event.motion.y < 510))
 					{
-<<<<<<< .mine
-						if(loaded_ == num_options_ - 1)
-							continue;
-
-=======
 						if (loaded_ == num_options_ - 1)
 							continue;
 
->>>>>>> .r133
 						loaded_ = num_options_ - 1;
 						Mix_PlayChannel(-1, music_, 0);
 					}

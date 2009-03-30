326d3c2664c3d7e1bed0367882c09fc9db9723a2
Kyle MacDonald <kmacdonald76@my.bcit.ca>
2009-03-30 21:39:27 +0000

    fixed some bugs going from tcp to udp back and forth.

--- networking/trunk/client-server/udp_server.cpp
+++ networking/trunk/client-server/udp_server.cpp
@@ -1,12 +1,13 @@
 #include "server.h"
+#define MAX_LEN 1024
 
 void start_udp_server() {
 	int sd, port = 8000, n, j;
 	socklen_t client_len;
-	char udpbuf[1024];
+	char udpbuf[MAX_LEN];
 	struct sockaddr_in udpserver, udpclient;
 	
-	printf("starting udp server.\n");
+	printf("starting udp server..\n");
 	fflush(stdout);
 
 	if((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -24,29 +25,27 @@ void start_udp_server() {
 	}
 
 	for(;;) {
-		printf("waiting udp..\n");
-		fflush(stdout);
 
 		client_len = sizeof(udpclient);
-		if((n = recvfrom(sd, udpbuf, 1024, 0, (struct sockaddr *)&udpclient, &client_len)) < 0) {
+		if((n = recvfrom(sd, udpbuf, MAX_LEN, 0, (struct sockaddr *)&udpclient, &client_len)) < 0) {
 			perror("Recv From");
 			exit(EXIT_FAILURE);
 		}
+
 		for(j=0; udpbuf[j] != '\n'; ++j);
 		udpbuf[++j] = '\0';
 
-		printf("sending udp..: %s\n", udpbuf);
-		fflush(stdout);
-
-		if(sendto(sd, udpbuf, n, 0, (struct sockaddr *)&udpclient, client_len) < 0) {
-			perror("Send To");
-			exit(EXIT_FAILURE);
-		}
-
 		if(strcmp(udpbuf, "quit\n")==0) {
 			close(sd);
-			printf("closing udp server\n");
-			break;
+			printf("closing udp server..\n");
+			return;
+		} else {
+			printf("udp: %s", udpbuf);
+			if(sendto(sd, udpbuf, n, 0, (struct sockaddr *)&udpclient, client_len) < 0) {
+				perror("Send To");
+				exit(EXIT_FAILURE);
+			}
 		}
 	}
 }
+

95987f4df9f66f9c9e2a41bee5d47178590c7ba8
Alin Albu <aalbu@my.bcit.ca>
2009-03-31 17:42:55 +0000

    Added POptMenu

---
+++ graphics/trunk/menu_pre_alpha_1/POptMenu.cpp
@@ -0,0 +1,273 @@
+/*
+ * POptMenu.cpp
+ *
+ *  Created on: 25-Mar-2009
+ *      Author: d3vil
+ */
+
+#include "POptMenu.h"
+
+POptMenu::POptMenu(std::string modelNames[], std::string fileNames[], std::string mfilename, std::string smfilename, SDL_Surface* screen)
+{
+	int i;
+	loaded_ = 0;
+	modelLoaded_ = 0;
+	this->screen_ = screen;
+	for(i = 0; i < num_options_; i++)
+	{
+		this->fileNames_[i] = fileNames[i];
+		if (!loadBackground(fileNames_[i], i))
+		{
+			printf("File %s did not load.\n", fileNames_[i].c_str());
+			exit(1);
+		}
+	}
+	for(i = 0; i < num_models_; i++)
+	{
+
+		this->modelNames_[i] = modelNames[i];
+		if (!loadModel(modelNames_[i], i))
+		{
+			printf("File %s did not load.\n", modelNames_[i].c_str());
+			exit(1);
+		}
+	}
+	this->setMusic(Mix_LoadMUS( mfilename.c_str()));
+	this->setNexMusic(Mix_LoadWAV(smfilename.c_str()));
+
+}
+
+POptMenu::~POptMenu() {
+	for(int i =0; i< num_options_; i++)
+	{
+		SDL_FreeSurface(backgrounds_[i]);
+	}
+	Mix_FreeMusic( music_);
+	Mix_FreeChunk(nexMusic_);
+}
+
+bool POptMenu::showModels()
+{
+	SDL_Rect offset;
+	SDL_Surface* temp;
+	int i, toLoad, result;
+	double center, left1, left2, left3, right1, right2, right3;
+	offset.y = screen_->h - 300;
+
+	center = screen_->w / 2;
+	left1  = 0.75 * center;
+	left2  = 0.5 * center;
+	left3  = 0.25 * center;
+
+	right1 = center + left3;
+	right2 = center + left2;
+	right3 = center + left1;
+
+	for(i = 0; i < 3; i++ )
+	{
+		toLoad = (modelLoaded_ - 3) + i;
+		if (toLoad  <  0)
+		{
+			continue;
+		}
+
+		switch (i){
+			case 0:
+			{
+				temp = zoomSurface(models_[toLoad], 0.7, 0.7, 1);
+				optimizeImg(temp);
+				offset.x = (int) left3 - (temp->w / 2);
+				result = SDL_BlitSurface( temp,0, screen_, &offset );
+				break;
+
+			}
+
+			case 1:
+			{
+				temp = zoomSurface(models_[toLoad], 0.8, 0.8, 1);
+				optimizeImg(temp);
+				offset.x = (int)left2 - (temp->w / 2) ;
+				result = SDL_BlitSurface( temp,0, screen_, &offset );
+				break;
+
+			}
+			case 2:
+			{
+				temp = zoomSurface(models_[toLoad], 0.9, 0.9, 1);
+				optimizeImg(temp);
+				offset.x = (int)left1 - (temp->w / 2);
+				result = SDL_BlitSurface( temp,0, screen_, &offset );
+				break;
+
+			}
+		}
+	}
+
+		for(i = 6; i > 3; i-- )
+		{
+			toLoad = (modelLoaded_ - 3) + i;
+			if (toLoad >= num_models_)
+			{
+				continue;
+			}
+
+		switch (i){
+			case 4:
+			{
+				temp = zoomSurface(models_[toLoad], 0.9, 0.9, 1);
+				optimizeImg(temp);
+				offset.x = (int)right1 - (temp->w / 2);;
+				result = SDL_BlitSurface( temp,0, screen_, &offset );
+				break;
+
+			}
+			case 5:
+			{
+				temp= zoomSurface(models_[toLoad], 0.8, 0.8, 1);
+				optimizeImg(temp);
+				offset.x = (int)right2 - (temp->w / 2);
+				result = SDL_BlitSurface( temp,0, screen_, &offset );
+				break;
+
+			}
+			case 6:
+			{
+				temp = zoomSurface(models_[toLoad], 0.7, 0.7, 1);
+				optimizeImg(temp);
+				offset.x = (int)right3 - (temp->w / 2);
+				result = SDL_BlitSurface( temp,0, screen_, &offset );
+				break;
+			}
+		}
+	}
+
+
+	offset.x= (int)center -(int) (models_[modelLoaded_]->w / 2);
+	result = SDL_BlitSurface( models_[modelLoaded_],0, screen_, &offset );
+
+	 if (result == 0)
+			return (SDL_Flip( screen_ ) != -1);
+		else
+			return false;
+}
+bool POptMenu::showloaded()
+{
+	return 	this->move(0,0) && 	this->showModels();
+	}
+
+bool POptMenu::move(int xStep, int yStep)
+{
+	SDL_Rect offset;
+	offset.x = xStep;
+	offset.y = yStep;
+	int i = SDL_BlitSurface( backgrounds_[loaded_],0, screen_, &offset );
+	return i == 0;
+}
+
+int POptMenu::start(SDL_Event event)
+{
+	loaded_ = 0;
+	if(!this->showloaded()) return 0;
+	printf("models loaded!\n");
+	while(true)
+	{
+		SDL_WaitEvent(&event);
+		if(event.type == SDL_KEYDOWN)
+		{
+			switch(event.key.keysym.sym)
+			{
+				case SDLK_UP:
+					Mix_PlayChannel(-1, nexMusic_, 0);
+					if(loaded_ == num_options_ -1){
+						loaded_ = 1;
+					}else{
+						++loaded_;
+					}
+					if(!this->showloaded()) return 0;
+					break;
+
+				case SDLK_DOWN:
+					Mix_PlayChannel(-1, nexMusic_, 0);
+					if(loaded_ < 2)
+					{
+						loaded_ = num_options_ -1;
+					}else{
+						--loaded_;
+					}
+					if(!this->showloaded()) return 0;
+					break;
+				case SDLK_LEFT:
+					 if(loaded_ != 2)
+						 break;
+					 if(!this->move(0,0)) return 0;
+					 this->setModelLoaded(this->getModelLoaded() - 1);
+					 this->showModels();
+					break;
+				case SDLK_RIGHT:
+					if(loaded_ != 2)
+						 break;
+					if(!this->move(0,0)) return 0;
+					this->setModelLoaded(this->getModelLoaded() + 1);
+					this->showModels();
+					break;
+				case SDLK_F11:
+					return 'f';
+				case SDLK_ESCAPE:
+					Mix_PlayChannel(-1, nexMusic_, 0);
+					return 1;
+				case SDLK_RETURN:
+					if(loaded_ == 0)
+						break;
+					Mix_PlayChannel(-1, nexMusic_, 0);
+					return loaded_;
+				default:
+				break;
+			}
+		}
+		else if (event.type == SDL_MOUSEMOTION)
+			{
+				if((event.motion.x >300) && (event.motion.y < 150))
+				{
+					if(loaded_ == num_options_ - 1)
+						continue;
+					loaded_ = num_options_ - 1;
+					Mix_PlayChannel(-1, nexMusic_, 0);
+				}else
+				if((event.motion.x < 400) && (event.motion.y < 300) && (event.motion.y > 150))
+				{
+
+					if(loaded_ == num_options_ - 2)
+						continue;
+
+					loaded_ = num_options_ - 2;
+					Mix_PlayChannel(-1, nexMusic_, 0);
+				}else
+				if((event.motion.x > 200) && (event.motion.y > 300) && (event.motion.y < 650))
+				{
+
+					if(loaded_ == num_options_ - 3)
+						continue;
+
+					loaded_ = num_options_ - 3;
+					Mix_PlayChannel(-1, nexMusic_, 0);
+				}else
+				if((event.motion.x < 200) && (event.motion.y > 700))
+				{
+
+					if (loaded_ == num_options_ - 4)
+						continue;
+					loaded_ = num_options_ - 4;
+					Mix_PlayChannel(-1, nexMusic_, 0);
+				}else
+				{
+					loaded_ = 0;
+				}
+			}
+		else if ((event.type == SDL_MOUSEBUTTONUP) && loaded_)
+		{
+			return loaded_;
+		}
+	}
+	return 0;
+
+}
---
+++ graphics/trunk/menu_pre_alpha_1/POptMenu.h
@@ -0,0 +1,163 @@
+/*
+ * POptMenu.h
+ *
+ *  Created on: 27-Mar-2009
+ *      Author: d3vil
+ */
+
+#ifndef POPTMENU_H_
+#define POPTMENU_H_
+#include "mainHeader.h"
+
+class POptMenu {
+public:
+	POptMenu(std::string modelNames[], std::string fileNames[], std::string mfilename, std::string smfilename, SDL_Surface* screen);
+	virtual ~POptMenu();
+
+	static const int num_options_ = 5;  //number of options
+	static const int num_models_ = 8;
+
+	int getNum_models() const{
+	        return num_models_;
+	    }
+
+    int getNum_options() const{
+        return num_options_;
+    }
+
+    std::string getFileNames(int index) const
+    {
+    	if (index >= num_options_ || index < 0)
+		{
+			return "";
+		}
+        return fileNames_[index];
+    }
+
+    void setFileNames(std::string fileName, int index)
+    {
+    	if (index >= num_options_ || index < 0)
+		{
+			return ;
+		}
+        this->fileNames_[index] = fileName;
+    }
+
+    SDL_Surface *getBackground(int index) const
+    {
+    	if (index >= num_options_ || index < 0)
+		{
+			return NULL;
+		}
+        return backgrounds_[index];
+    }
+
+    void setBackground(SDL_Surface *background,int index)
+    {
+    	if (index >= num_options_ || index < 0)
+		{
+			return ;
+		}
+        this->backgrounds_[index] = background;
+    }
+
+    bool loadBackground(std::string filename,int index)
+    {
+    	if (index >= num_options_ || index < 0)
+    	{
+    		return false;
+    	}
+    	backgrounds_[index]= load_image(filename.c_str());
+    	return backgrounds_[index] != NULL;
+    }
+
+    bool loadModel(std::string filename, int index)
+   {
+	if (index >= num_models_ || index < 0)
+	{
+		return false;
+	}
+	models_[index]= load_image(filename.c_str());
+	return models_[index] != NULL;
+   }
+
+    SDL_Surface *getScreen() const
+    {
+        return screen_;
+    }
+
+    void setScreen(SDL_Surface *screen_)
+    {
+        this->screen_ = screen_;
+    }
+
+    Mix_Chunk *getNexMusic() const
+    {
+        return nexMusic_;
+    }
+
+    void setNexMusic(Mix_Chunk *nexMusic_)
+    {
+        this->nexMusic_ = nexMusic_;
+    }
+
+    Mix_Music *getMusic() const
+    {
+        return music_;
+    }
+
+    void setMusic(Mix_Music *music_)
+    {
+        this->music_ = music_;
+    }
+
+
+    bool showModels();
+    bool showloaded();
+    bool move(int xStep, int yStep);
+    int start(SDL_Event event);
+
+    int getModelLoaded() const
+    {
+        return modelLoaded_;
+    }
+
+    void setModelLoaded(int modelLoaded)
+    {
+    	if (modelLoaded < 0){
+    		this->modelLoaded_ = 0;
+    		return;
+    	}
+		if (modelLoaded >= num_models_ ){
+			this->modelLoaded_ = num_models_ -1;
+			return;
+		}
+
+        this->modelLoaded_ = modelLoaded;
+    }
+
+    std::string getModelNames(int index) const
+    {
+        return modelNames_[index];
+    }
+
+    void setModelNames(std::string modelName, int index)
+    {
+        this->modelNames_[index] = modelName;
+    }
+
+private:
+	int loaded_;
+	int modelLoaded_;
+	std::string fileNames_[num_options_]; // background files
+	std::string modelNames_[num_models_]; // model files
+	SDL_Surface* backgrounds_[num_options_];//the actual loaded backgrounds
+	SDL_Surface* models_[num_models_];
+	SDL_Surface* screen_;
+	Mix_Music *music_;
+	Mix_Chunk *nexMusic_;
+
+};
+
+
+#endif /* POPTMENU_H_ */

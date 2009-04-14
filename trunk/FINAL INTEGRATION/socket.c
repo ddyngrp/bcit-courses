63b7ce69a14251481ac463b44b14a45ffae896f1
Steffen L. Norgren <ironix@trollop.org>
2009-04-14 11:44:07 +0000

    Renamed the folder.

---
+++ trunk/FINAL INTEGRATION/socket.c
@@ -0,0 +1,393 @@
+/*-----------------------------------------------------------------------------
+--	SOURCE FILE:	socket.c
+--
+--	PROGRAM:		CommAudio.exe
+--
+--	FUNCTIONS:		
+--
+--
+--	DATE:			
+--
+--	DESIGNERS:		Jerrod Hudson
+--	PROGRAMMERS:	Jerrod Hudson, Jaymz Boilard, Steffen L. Norgren
+--
+--	NOTES:	
+-----------------------------------------------------------------------------*/
+
+#include "socket.h"
+
+/*-----------------------------------------------------------------------------
+--	FUNCTION:		sockClose
+--
+--	DATE:			2009-03-29
+--
+--	REVISIONS:		2009-04-13 - Steffen, added thread termination.
+--
+--	DESIGNER(S):	Jerrod Hudson
+--	PROGRAMMER(S):	Jerrod Hudson, Steffen L. Norgren
+--
+--	INTERFACE:		sockClose(HWND hwnd, WPARAM wParam, LPARAM lParam)
+--						HWND hwnd: The handle to the calling window.
+--						WPARAM wParam: The socket we wish to close
+--						LPARAM lParam: -1 if we're not in multicast.
+--
+--	RETURNS:		void
+--
+--	NOTES: Closes the socket and terminates any associated threads if we're
+--	in multicast mode.
+-----------------------------------------------------------------------------*/
+void sockClose(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	if (lParam != -1) {
+		shutdown(wParam, 2);
+		closesocket(wParam);
+	}
+	else if (ci.request != MULTI_STREAM) { /* End our currently running thread */
+		if(streamThread != NULL)
+			TerminateThread(streamThread,0);
+
+		ci.request = 0;
+
+		Sleep(200);
+	}
+}
+
+/*-----------------------------------------------------------------------------
+--	FUNCTION:		tcp_sockAccept
+--
+--	DATE:			2009-03-29
+--
+--	REVISIONS:		
+--
+--	DESIGNER(S):	Jerrod Hudson
+--	PROGRAMMER(S):	Jerrod Hudson, Steffen L. Norgren
+--
+--	INTERFACE:		tcp_sockAccept(HWND hwnd, WPARAM wParam, LPARAM lParam)
+--						HWND hwnd: The handle to the calling window.
+--						WPARAM wParam: The socket we wish to close
+--						LPARAM lParam: unused
+--						
+--
+--	RETURNS:		void
+--
+--	NOTES: Used by the server to accept an incoming request by a client.
+-----------------------------------------------------------------------------*/
+void tcp_sockAccept(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	SOCKET sd_acc;
+
+	if(wParam == INVALID_SOCKET)
+		return;
+
+	sd_acc = accept(wParam, NULL, NULL);
+
+	if(sd_acc == INVALID_SOCKET) {
+		MessageBox(ghWndMain, (LPCSTR)"Unable to create accept socket!",
+			(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+	}
+}
+
+/*-----------------------------------------------------------------------------
+--	FUNCTION:		tcp_sockConnect
+--
+--	DATE:			2009-03-29
+--
+--	REVISIONS:		
+--
+--	DESIGNER(S):	Jerrod Hudson
+--	PROGRAMMER(S):	Jerrod Hudson
+--
+--	INTERFACE:		tcp_sockConnect(HWND hwnd, WPARAM wParam, LPARAM lParam)
+--						HWND hwnd: The handle to the calling window.
+--						WPARAM wParam: The socket we wish to close
+--						LPARAM lParam: unused
+--
+--	RETURNS:		void
+--
+--	NOTES: Checks for an error on connection and prints it in a MessageBox.
+-----------------------------------------------------------------------------*/
+void tcp_sockConnect(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	int errNum;
+	char *error;
+
+	if((errNum = WSAGETSELECTERROR(lParam)) == 0)
+		return;
+
+	switch(errNum)
+	{
+	case WSAECONNREFUSED:
+		error = "Server full.  Try again later.\0";
+		break;
+
+	case WSAEISCONN:
+		error = "Already connected!\0";
+		break;
+
+	case WSAETIMEDOUT:
+		error = "Connection attempt timed out.\0";
+		break;
+
+	default:
+		error = "Connection error!\0";
+		break;
+	}
+
+	MessageBox(ghWndMain, error, (LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+}
+
+/*-----------------------------------------------------------------------------
+--	FUNCTION:		tcp_sockRead
+--
+--	DATE:			2009-03-29
+--
+--	REVISIONS:		2009-04-06 - Jaymz, Added the SINGLE_STREAM case which will
+--								 call the function receiveStream().
+--
+--	DESIGNER(S):	Jerrod Hudson
+--	PROGRAMMER(S):	Jerrod Hudson, Jaymz Boilard, Steffen L. Norgren
+--
+--	INTERFACE:		tcp_sockRead(HWND hwnd, WPARAM wParam, LPARAM lParam)
+--						HWND hwnd: The handle to the calling window.
+--						WPARAM wParam: The socket we wish to close
+--						LPARAM lParam: unused
+--
+--	RETURNS:		void
+--
+--	NOTES: Reads from a socket asyncronously when we get a message in.
+-----------------------------------------------------------------------------*/
+void tcp_sockRead(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	char buffer[FILE_BUFF_SIZE];
+	int bytesRead;
+	int cSize;
+	static BOOL firstTime = TRUE;
+	ci.tcpSock2 = wParam;
+
+	if(wParam == INVALID_SOCKET)
+		return;
+
+	cSize = sizeof(SOCKADDR);
+	memset(buffer, '\0', FILE_BUFF_SIZE);
+
+	if(ci.behaviour == SERVER)
+	{
+		if(ci.request == SINGLE_UP)
+		{
+			strcpy_s(ci.DLfileName, sizeof(ci.DLfileName), "upload_file.wav");
+			client_download(wParam); /* The client's download is the inverse of the server's upload */
+			return;
+		}
+
+		/* First packet we get in, which specifies the type of operation that the user wants to perform */
+		if(recv(wParam, buffer, sizeof(buffer), 0) == -1)
+		{
+			if (WSAGetLastError() != WSAEWOULDBLOCK)
+			{
+				MessageBox(ghWndMain, (LPCSTR)"recv() failed.",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+				closesocket(wParam);
+			}
+		}
+
+		if(ci.request == SINGLE_DL) //need to set request to 0 when we click on server & gray the other request items
+			server_download(wParam, buffer); /* Go right to sending the file. */
+		else if(ci.request == SINGLE_STREAM)
+		{
+			strcpy_s(ci.DLfileName, sizeof(ci.DLfileName), buffer);
+
+			if(streamThread != NULL)
+				TerminateThread(streamThread,0);
+
+			streamThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)sendStream, (LPVOID)wParam, 0, 0);
+			if(streamThread == NULL)
+			{
+				MessageBox(ghWndMain, (LPCSTR)"Thread creation failed",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+				ExitProcess(1);
+			}
+		}
+		else if(ci.request == MICROPHONE) {
+			waveOutWrite(hWaveOut, (WAVEHDR *)buffer, sizeof(WAVEHDR));
+		}
+
+		/* Set the type according to the first packet */
+		if (ci.request != MULTI_STREAM) {
+			if(strcmp(buffer, "Single Download") == 0)
+			{
+				ci.request = SINGLE_DL;
+				sendFileList(wParam);
+			}
+			else if(strcmp(buffer, "Single Upload") == 0) {
+				ci.request = SINGLE_UP;
+			}
+			else if(strcmp(buffer, "Stream") == 0)
+			{
+				ci.request = SINGLE_STREAM;
+				sendFileList(wParam);
+			}
+			else if(strcmp(buffer, "Microphone") == 0)
+			{
+				ci.request = MICROPHONE;
+				//prepareMicPlay();
+				//Sleep(100);
+				//mic_record_beg();
+				/* Put receiveStream thread here */
+				streamThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)receiveMicThread,
+					(LPVOID)wParam, 0, 0);
+			}
+		}
+		else {
+			if (strcmp(buffer, "Multicast") == 0)
+			{
+				ci.request = MULTI_STREAM;
+			}
+			else {
+				strcpy_s(buffer, FILE_BUFF_SIZE, "NO");
+				send(wParam, buffer, strlen(buffer) /*sizeof(buffer)*/, 0);
+			}
+		}
+	}
+	else if (ci.behaviour == CLIENT)
+	{
+		if(ci.request == SINGLE_DL)
+		{
+			Sleep(1);
+			client_download(wParam);
+		}
+		else if (ci.request == SINGLE_UP) {
+			if((bytesRead = recv(wParam, buffer, FILE_BUFF_SIZE, 0)) == -1)
+			{
+				if (WSAGetLastError() != WSAEWOULDBLOCK)
+				{
+					MessageBox(ghWndMain, (LPCSTR)"WSARecv() failed.",
+						(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+					closesocket(wParam);
+				}
+			}
+			if (strncmp(buffer, "NO", 2) == 0) {
+				MessageBox(ghWndMain, (LPCSTR)"Server says NO!",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+				ExitProcess(1);
+			}
+		}
+		else if(ci.request == SINGLE_STREAM)
+		{
+			if((bytesRead = recv(wParam, buffer, FILE_BUFF_SIZE, 0)) == -1)
+			{
+				if (WSAGetLastError() != WSAEWOULDBLOCK)
+				{
+					MessageBox(ghWndMain, (LPCSTR)"WSARecv() failed.",
+						(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+					closesocket(wParam);
+				}
+			}
+			if (strncmp(buffer, "NO", 2) == 0) {
+				MessageBox(ghWndMain, (LPCSTR)"Server says NO!",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+				ExitProcess(1);
+			}
+			receiveFileList(wParam, buffer);
+		}
+		else if(ci.request == MICROPHONE)
+		{
+			if((bytesRead = recv(wParam, buffer, FILE_BUFF_SIZE, 0)) == -1)
+			{
+				if (WSAGetLastError() != WSAEWOULDBLOCK)
+				{
+					MessageBox(ghWndMain, (LPCSTR)"WSARecv() failed.",
+						(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+					closesocket(wParam);
+				}
+			}
+			if (strncmp(buffer, "NO", 2) == 0) {
+				MessageBox(ghWndMain, (LPCSTR)"Server says NO!",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+				ExitProcess(1);
+			}
+		}	
+	}
+	initButtons();
+}
+
+
+/*-----------------------------------------------------------------------------
+--	FUNCTION:		tcp_sockWrite
+--
+--	DATE:			2009-03-29
+--
+--	REVISIONS:		2009-03-26 - Jerrod, Added a check for the request type, which changes the
+--								 the outbound buffer correspondingly.
+--							   - Jaymz, Changed the name to tcp_sockWriteet, since we will only
+--								 be using this one for sending control messages.  A
+--								 separate one will be used for UDP transfers.
+--					2009-04-06 - Jaymz, Added a memset to clear all the buffers due to an error.
+--
+--	DESIGNER(S):	Jerrod Hudson
+--	PROGRAMMER(S):	Jerrod Hudson, Jaymz Boilard, Steffen L. Norgren
+--
+--	INTERFACE:		tcp_sockWrite(HWND hwnd, WPARAM wParam, LPARAM lParam)
+--						HWND hwnd: The handle to the calling window.
+--						WPARAM wParam: The socket we wish to close
+--						LPARAM lParam: unused
+--
+--	RETURNS:		void
+--
+--	NOTES: Writes to a socket, used for the control channel.
+-----------------------------------------------------------------------------*/
+void tcp_sockWrite(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	char	buffer[FILE_BUFF_SIZE];
+	TCHAR	fileName[FILE_PATH_SIZE], pathName[FILE_PATH_SIZE];
+
+	memset(buffer, '\0', FILE_BUFF_SIZE);
+	memset(fileName, '\0', FILE_PATH_SIZE);
+	memset(pathName, '\0', FILE_PATH_SIZE);
+
+	if(ci.behaviour == CLIENT)
+	{
+		if(ci.request == SINGLE_DL)
+		{
+			strcpy_s(buffer, FILE_BUFF_SIZE, "Single Download");
+			Sleep(10);
+		}
+		else if(ci.request == SINGLE_UP)
+		{
+			strcpy_s(buffer, FILE_BUFF_SIZE, "Single Upload");
+			Sleep(10);
+			browseFiles(hwnd, fileName, pathName);
+		}
+		else if(ci.request == SINGLE_STREAM)
+			strcpy_s(buffer, FILE_BUFF_SIZE, "Stream");
+		else if(ci.request == MULTI_STREAM)
+			strcpy_s(buffer, FILE_BUFF_SIZE, "Multicast");
+		else if(ci.request == MICROPHONE)
+			strcpy_s(buffer, FILE_BUFF_SIZE, "Microphone");
+
+		if(send(wParam, buffer,strlen(buffer) /*sizeof(buffer)*/, 0) == -1)
+		{
+			if (WSAGetLastError() != WSAEWOULDBLOCK)
+			{
+				MessageBox(ghWndMain, (LPCSTR)"send() failed.",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+				closesocket(wParam);
+			}
+		}
+
+		if(ci.request == SINGLE_UP)
+		{
+			Sleep(1);
+			server_download(wParam, fileName); /* Client's upload is inverse of server's download */
+		}
+		else if(ci.request == MICROPHONE) /* We should only get here once, the data sends are non-async */
+		{
+			Sleep(100);
+			mic_record_beg(); //start the microphone input
+		}
+	}
+	else if (ci.behaviour == SERVER) {
+		MessageBox(ghWndMain,
+			(LPCSTR)"Warning: Asyncronous TCP write called by the serverk (should not happen).\n",
+			(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+	}
+}
---
+++ trunk/FINAL INTEGRATION/win_events.c
@@ -0,0 +1,419 @@
+/*-----------------------------------------------------------------------------
+--	SOURCE FILE:	win_events.c
+--
+--	PROGRAM:		CommAudio.exe
+--
+--	FUNCTIONS:		
+--
+--
+--	DATE:			
+--
+--	DESIGNERS:		
+--	PROGRAMMERS:	
+--
+--	NOTES:	
+-----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+--	SOURCE FILE:	win_events.c - A general purpose event handler for menus and dialogs.
+--
+--	PROGRAM:		music_streamer.exe
+--
+--	FUNCTIONS:		MenuDispatch(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+--					MainDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+--
+--	DATE:			March 16, 2009
+--
+--	REVISIONS:		
+--
+--	DESIGNERS:		Steffen L. Norgren
+--
+--	PROGRAMMER:		Steffen L. Norgren
+--
+--	NOTES:	This is a simple menu event handler. It responds to user menu and dialog
+--			selections and takes appropriate action depending on the user's input.
+--			Remember to add "WS2_32.Lib" to the project source list.
+---------------------------------------------------------------------------------------*/
+#include "win_events.h"
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnClose
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		(Date and Description)
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson
+--
+--		INTERFACE:		void OnClose(HWND hwnd)
+--							HWND hwnd: Handle to the window to close
+--
+--		RETURNS:		void
+--
+--		NOTES:			Used to close a window.
+------------------------------------------------------------------------*/
+void OnClose(HWND hwnd)
+{
+	PostQuitMessage(0);
+}
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnCommand
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		March 26 - Added code to make the menu items properly
+--								   gray, ungray, and add check marks as needed.
+--						March 29 - Moved the code for ID_FILE_LOCAL from
+--								   Menu_Dispatch to here.
+--						April 4  - 
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson & Jaymz Boilard
+--
+--		INTERFACE:		void OnCommand(HWND hwnd, int id, HWND hwndCtl,
+--						UINT codeNotify)
+--							HWND hwnd: Handle to the current window
+--							int id: The message ID
+--							HWND hwndCtl: Handle to the window control
+--							UINT codeNotify: Notify code
+--
+--		RETURNS:		void
+--
+--		NOTES:			This function performs actions based on the
+--						message ID.  Currently, only menu items are
+--						processed here.
+------------------------------------------------------------------------*/
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	TCHAR fileName[FILE_BUFF_SIZE], pathName[FILE_BUFF_SIZE];
+	char ipAddr[TEMP_BUFF_SIZE] = "IP: ";
+
+	switch(id)
+	{
+	case ID_FILE_CONNECT:
+		if(ci.behaviour == SERVER)
+		{
+			setup_server(hwnd, SOCK_STREAM);
+			setup_server(hwnd, SOCK_DGRAM);
+
+			if (ci.request == MULTI_STREAM) {
+				sendFileList(0);
+			}
+
+			if(ci.tcpSocket == INVALID_SOCKET)
+			{
+				MessageBox(ghWndMain, (LPCSTR)"Invalid Socket!",
+					(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+			}
+		}
+		else if(ci.behaviour == CLIENT) 
+		{
+			setup_client(hwnd, SOCK_STREAM);
+			setup_client(hwnd, SOCK_DGRAM);
+
+			if (ci.request == MULTI_STREAM) {
+				if(streamThread != NULL) {
+					TerminateThread(streamThread,0);
+				}
+
+				Sleep(100);
+
+				if((streamThread = CreateThread(NULL, 0, 
+					(LPTHREAD_START_ROUTINE)receiveStream, (LPVOID)ci.udpSocket, 0, 0)) == NULL)
+				{
+					MessageBox(NULL,"Thread creation failed",NULL,MB_OK);
+					ExitProcess(1);
+				}
+			}
+			else {
+				connect(ci.tcpSocket, (SOCKADDR *)&remote, sizeof(remote));
+
+				if(ci.tcpSocket == INVALID_SOCKET)
+				{
+					if(WSAGetLastError() != WSAEWOULDBLOCK) {
+						MessageBox(ghWndMain, (LPCSTR)"Unable to connect to server!",
+							(LPCSTR)"Error!", MB_OK | MB_ICONSTOP);
+					}
+				}
+			}
+		}
+
+		connectActions();
+		initButtons();
+		SendMessage(ghStatus,SB_SETTEXT,(WPARAM)parts[0],(LPARAM)"Status: Connected");
+		break;
+
+	case ID_FILE_DISCONNECT:
+		/* Stop running threads */
+		if(streamThread != NULL)
+			TerminateThread(streamThread,0);
+
+		Sleep(200);
+
+		/* Close open sockets */
+		sockClose(hwnd, ci.tcpSocket, 0);
+		sockClose(hwnd, ci.udpSocket, 0);
+
+		ci.behaviour = 0;
+		ci.request = 0;
+
+		disconnectActions();
+		setActions();
+		initButtons();
+		ClearList();
+
+		SendMessage(ghStatus, SB_SETTEXT, (WPARAM)parts[0], (LPARAM)"Status: Disconnected");
+		SendMessage(ghStatus, SB_SETTEXT, (WPARAM)parts[1], (LPARAM)"Mode: ");
+		SendMessage(ghStatus, SB_SETTEXT, (WPARAM)parts[2], (LPARAM)"IP: ");
+		break;
+
+	case ID_FILE_EXIT:
+		PostMessage(hwnd, WM_CLOSE, 0, 0);
+		break;
+
+	case ID_MODE_CLIENT:
+		DialogBox(ghInst, MAKEINTRESOURCE(IDD_CLIENT), hwnd, (DLGPROC)ClientProc);
+		ci.behaviour = CLIENT;
+
+		checkMenuItem(0);
+		setActions();
+
+		SendMessage(ghStatus,SB_SETTEXT,(WPARAM)parts[1],(LPARAM)"Mode: Client");
+		strcat_s(ipAddr, sizeof(ipAddr), ci.ip);
+		SendMessage(ghStatus,SB_SETTEXT,(WPARAM)parts[2],(LPARAM)ipAddr);
+		break;
+
+	case ID_MODE_SERVER:
+		DialogBox(ghInst, MAKEINTRESOURCE(IDD_SERVER), hwnd, (DLGPROC)ServerProc);
+		ci.behaviour = SERVER;
+
+		checkMenuItem(0);
+		setActions();
+
+		SendMessage(ghStatus,SB_SETTEXT,(WPARAM)parts[1],(LPARAM)"Mode: Server");
+		break;
+
+	/* Note: These menu item checks can be put into a loop within a function */
+	case ID_SINGLE_DL:
+		checkMenuItem(ID_SINGLE_DL);
+		ci.request = SINGLE_DL;
+		break;
+	case ID_SINGLE_UP:
+		checkMenuItem(ID_SINGLE_UP);
+		ci.request = SINGLE_UP;
+		break;
+	case ID_SINGLE_STREAM:
+		checkMenuItem(ID_SINGLE_STREAM);
+		ci.request = SINGLE_STREAM;
+		break;
+	case ID_MULTI_STREAM:
+		checkMenuItem(ID_MULTI_STREAM);
+		ci.request = MULTI_STREAM;
+		break;
+	
+	case ID_2WAY_MICROPHONE:
+		checkMenuItem(ID_2WAY_MICROPHONE);
+		ci.request = MICROPHONE;
+		break;
+
+	case ID_FILE_LOCAL:
+		localSong_Stop();
+		EnableWindow(GetDlgItem(ghDlgMain, IDC_BTN_PAUSE), TRUE);
+		SetWindowText(GetDlgItem(ghDlgMain, IDC_BTN_PAUSE), (LPCSTR)"Pause");
+        busyFlag = LOCALPLAY;
+        memset(fileName, 0, FILE_BUFF_SIZE);
+	    memset(pathName, 0, FILE_BUFF_SIZE);
+        browseFiles(hwnd, fileName, pathName);
+		if(localSong_Init(hwnd, fileName) == FALSE) {
+            busyFlag = 0; /* if we failed to open file */
+		}
+        break;
+
+	case ID_HELP_ABOUT:
+		MessageBox(ghWndMain, (LPCSTR)"COMP 4985 - Assignment #3, Comm Audio\n\n"
+			"Programmers:\n"
+			"\tJaymz Boilard\n"
+			"\tJerrod Hudson\n"
+			"\tBrendan Neva\n"
+			"\tSteffen L. Norgren\n",
+			(LPCSTR)"About CommAudio", MB_OK | MB_ICONINFORMATION);
+		break;
+
+	default:
+		break;
+	}
+}
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnCreate
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		(Date and Description)
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson
+--
+--		INTERFACE:		int OnCreate(HWND hwnd,
+--						LPCREATESTRUCT lpCreateStruct)
+--							HWND hwnd: Handle to the window to close
+--							LPCREATESTRUCT lpCreateStruct: Initialization
+--							parameters for the window
+--
+--		RETURNS:		TRUE if the window was created successfully,
+--						FALSE otherwise
+--
+--		NOTES:			When called, sets up the window.
+------------------------------------------------------------------------*/
+int OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+{
+	InitWindow(hwnd);
+
+	fileInit(hwnd); //set up file browsing
+	busyFlag = 0;
+	memset((char *)&ci, 0, sizeof(connectInfo));
+	ci.tcp_port	= TCP_PORT;
+	ci.udp_port = UDP_PORT;
+
+	ghMenu = GetMenu(hwnd);
+	initMenu();
+
+	/* Create status bar & initialize fields */
+	ghStatus = CreateStatusWindow(WS_CHILD|WS_VISIBLE, "Status: Disconnected", hwnd, STATUS_BAR);
+	SendMessage(ghStatus,SB_SETPARTS,(WPARAM)nParts,(LPARAM)&width);
+	SendMessage(ghStatus, SB_SETTEXT,(WPARAM)parts[1],(LPARAM)"Mode:");
+	SendMessage(ghStatus, SB_SETTEXT,(WPARAM)parts[2],(LPARAM)"IP:");
+	return TRUE;
+}
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnDestroy
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		(Date and Description)
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson
+--
+--		INTERFACE:		void OnDestroy(HWND hwnd)
+--							HWND hwnd: Handle to the window to close
+--
+--		RETURNS:		void
+--
+--		NOTES:			Destroys the window, killing the program.
+------------------------------------------------------------------------*/
+void OnDestroy(HWND hwnd)
+{
+	PostQuitMessage(0);
+}
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnPaint
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		(Date and Description)
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson
+--
+--		INTERFACE:		void OnPaint(HWND hwnd)
+--							HWND hwnd: Handle to the window to paint
+--
+--		RETURNS:		void
+--
+--		NOTES:			Redraws the window.
+------------------------------------------------------------------------*/
+void OnPaint(HWND hwnd)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	hdc = BeginPaint(hwnd, &ps);
+	EndPaint(hwnd, &ps);
+}
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnSize
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		(Date and Description)
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson
+--
+--		INTERFACE:		void OnSize(HWND hwnd, UINT state, int cy, int cx)
+--							HWND hwnd: Handle to the window to paint
+--							UINT state: State of the window
+--							int cy: height of the client window
+--							int cx: width of the client window
+--
+--		RETURNS:		void
+--
+--		NOTES:			Operations to perform when the window is resized.
+------------------------------------------------------------------------*/
+void OnSize(HWND hwnd, UINT state, int cy, int cx)
+{
+	/* Main window size has changed, need to resize child windows */
+	WORD wTop = 0;
+	WORD wHeight = cx;
+	WORD wWidth = cy;
+	RECT wRect;
+
+	GetWindowRect(ghWndMain, &wRect);
+
+	/* Put Window in the middle */
+	MoveWindow(ghDlgMain, 0, wTop, wWidth, wHeight, TRUE);
+}
+
+/*------------------------------------------------------------------------
+--		FUNCTION:		OnTCPSocket
+--
+--		DATE:			February 24, 2009
+--
+--		REVISIONS:		(Date and Description)
+--
+--		DESIGNER:		Jerrod Hudson
+--		PROGRAMMER:		Jerrod Hudson
+--
+--		INTERFACE:		void OnTCPSocket(HWND hwnd, WPARAM wParam,
+--						LPARAM lParam)
+--						HWND hwnd: Handle to the window to paint
+--						WPARAM wParam: Socket descriptor
+--						LPARAM lParam: Operation flags
+--
+--		RETURNS:		void
+--
+--		NOTES:			Performs flag specific socket operations.
+------------------------------------------------------------------------*/
+void OnTCPSocket(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	switch(WSAGETSELECTEVENT(lParam))
+	{
+	case FD_ACCEPT:
+		tcp_sockAccept(hwnd, wParam, lParam);
+		break;
+
+	case FD_CLOSE:
+		sockClose(hwnd, wParam, -1);
+		break;
+	
+	case FD_CONNECT:
+		tcp_sockConnect(hwnd, wParam, lParam);
+		break;
+
+	case FD_READ:
+		tcp_sockRead(hwnd, wParam, lParam);
+		break;
+
+	case FD_WRITE:
+		tcp_sockWrite(hwnd, wParam, lParam);
+		break;
+
+	default:
+		break;
+	}
+}

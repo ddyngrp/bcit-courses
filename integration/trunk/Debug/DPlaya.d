57fb55e1cb9a1bcec5ccda67c9c413315f2b5844
Alin Albu <aalbu@my.bcit.ca>
2009-04-15 03:49:53 +0000

    I think I added the good stuff

---
+++ integration/trunk/Debug/DPlaya.d
@@ -0,0 +1,12 @@
+DPlaya.d DPlaya.o: ../DPlaya.cpp ../DPlaya.h ../headerMain.h \
+  ../Map/user_map.h ../Map/init.h ../Map/init.h
+
+../DPlaya.h:
+
+../headerMain.h:
+
+../Map/user_map.h:
+
+../Map/init.h:
+
+../Map/init.h:
---
+++ integration/trunk/Debug/logic.d
@@ -0,0 +1,18 @@
+logic.d logic.o: ../logic.cpp ../logic.h ../general.h ../Map/init.h \
+  ../Map/user_map.h ../Map/init.h ../DPlaya.h ../headerMain.h ../defs.h
+
+../logic.h:
+
+../general.h:
+
+../Map/init.h:
+
+../Map/user_map.h:
+
+../Map/init.h:
+
+../DPlaya.h:
+
+../headerMain.h:
+
+../defs.h:

12efdedfe011af9716546c12be6417ae484f58cc
Steffen L. Norgren <ironix@trollop.org>
2009-05-01 20:52:20 +0000

    Created a changelog and slightly modified formatting in main.c

--- trunk/spry/main.c
+++ trunk/spry/main.c
@@ -9,7 +9,7 @@
 static void
 destroy_cb (GtkWidget* widget, gpointer data)
 {
-    gtk_main_quit ();
+	gtk_main_quit ();
 }
 
 /* This is the main function called when the program is started. */
@@ -21,11 +21,11 @@ main (int argc, char* argv[])
 	GtkWidget* main_window;
 	
 	/* grap gtk-specific arguments and apply accordingly */
-    gtk_init (&argc, &argv);
+	gtk_init (&argc, &argv);
 	
 	/* check for g_thread support */
-    if (!g_thread_supported ()) {
-        g_thread_init (NULL);
+	if (!g_thread_supported ()) {
+		g_thread_init (NULL);
 	}
 	
 	/* verify address was given */
@@ -35,28 +35,28 @@ main (int argc, char* argv[])
 	}
 
 	/* create main window */
-    main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_default_size (GTK_WINDOW (main_window), 800, 600);
-    gtk_widget_set_name (main_window, "Spry");
-    g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (destroy_cb), NULL);
+	main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size (GTK_WINDOW (main_window), 800, 600);
+	gtk_widget_set_name (main_window, "Spry");
+	g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (destroy_cb), NULL);
 	
 	/* create scrolled window (viewport) and add to main window*/
 	scrolled_window = gtk_scrolled_window_new (NULL, NULL);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 	gtk_container_add (GTK_CONTAINER (main_window), scrolled_window);
 
 	/* create web view and add to scrolled window */
 	web_view = WEBKIT_WEB_VIEW (webkit_web_view_new ());
-    gtk_container_add (GTK_CONTAINER (scrolled_window), GTK_WIDGET (web_view));
-    
+	gtk_container_add (GTK_CONTAINER (scrolled_window), GTK_WIDGET (web_view));
+	
 	/* load web page */
-    webkit_web_view_open (web_view, (gchar*)argv[1]);
+	webkit_web_view_open (web_view, (gchar*)argv[1]);
 
 	/* make everything visible and start gtk */
-    gtk_widget_grab_focus (GTK_WIDGET (web_view));
-    gtk_widget_show_all (main_window);
-    gtk_main ();
+	gtk_widget_grab_focus (GTK_WIDGET (web_view));
+	gtk_widget_show_all (main_window);
+	gtk_main ();
 
 	/* done */
-    return 0;
+	return 0;
 }

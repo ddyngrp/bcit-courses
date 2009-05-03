59fccae7f7ce9a49871405b3e6af5bbf215f0ff3
Steffen L. Norgren <ironix@trollop.org>
2009-05-03 14:13:34 +0000

    Created an Anjuta project to aid in development of the project. Anjuta is simply an IDE, which you can install via apt-get install anjuta. The reasoning for this is to simplify creation of configure and install scripts as well as some automatic dependency resolution.
    Created callbacks.c/h to house all Gtk+ GUI callback functions.
    
    Created standard GNU project layout and added LGPL headers to all
    relavant files.
    
    See the README file for how to compile via command line.
    
    * main.c: updated includes headers
    * callback.c: created to handle Gtk+ GUI event signals
    * callback.h: created for callback function definitions

---
+++ trunk/src/callbacks.c
@@ -0,0 +1,33 @@
+/*
+ * callbacks.c
+ * Copyright (C) 2009 Doug Penner <darwinsurvivor@gmail.com>
+ *                    Brendan Neva <bneva1@my.bcit.ca>
+ *                    Steffen L. Norgren <ironix@trollop.org>
+ *                    Eddie Zhang <edisonhammer@gmail.com>
+ * 
+ * callbacks.c is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * callbacks.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "callbacks.h"
+
+
+void
+destroy (GtkWidget *widget, gpointer data)
+{
+	gtk_main_quit ();
+}
---
+++ trunk/src/main.c
@@ -0,0 +1,95 @@
+/*
+ * main.c
+ * Copyright (C) 2009 Doug Penner <darwinsurvivor@gmail.com>
+ *                    Brendan Neva <bneva1@my.bcit.ca>
+ *                    Steffen L. Norgren <ironix@trollop.org>
+ *                    Eddie Zhang <edisonhammer@gmail.com>
+ * 
+ * main.c is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * main.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <config.h>
+
+#include <gtk/gtk.h>
+#include <webkit/webkit.h>
+
+#include "callbacks.h"
+
+
+/**
+ * main:
+ * @argc: number of arguments passed in
+ * @argv: the arguments passed via the command line
+ *
+ * Creates a new browser window that points to a URL that was passed
+ * in cia @argv
+ *
+ * Returns: %0 if the operation succeeded, else %1.
+ **/
+int
+main (int argc, char *argv[])
+{
+	GtkWidget* scrolled_window;
+	GtkWidget* main_window;
+	WebKitWebView* web_view;
+
+	/* sets the gtk locale for the current system */
+	gtk_set_locale ();
+
+	/* grap gtk-specific arguments and apply accordingly */
+	gtk_init (&argc, &argv);
+
+	/* check for g_thread support */
+	if (!g_thread_supported ()) {
+		g_thread_init (NULL);
+	}
+
+	/* verify address was given */
+	if (argc < 2) {
+		g_print("No Address Specified\n");
+		return 1;
+	}
+
+	/* create main window */
+	main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size (GTK_WINDOW (main_window), 800, 600);
+	gtk_widget_set_name (main_window, "Spry");
+	g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (destroy), NULL);
+	
+	/* create scrolled window (viewport) and add to main window*/
+	scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_container_add (GTK_CONTAINER (main_window), scrolled_window);
+
+	/* create web view and add to scrolled window */
+	web_view = WEBKIT_WEB_VIEW (webkit_web_view_new ());
+	gtk_container_add (GTK_CONTAINER (scrolled_window), GTK_WIDGET (web_view));
+	
+	/* load web page */
+	webkit_web_view_open (web_view, (gchar*)argv[1]);
+
+	/* make everything visible and start gtk */
+	gtk_widget_grab_focus (GTK_WIDGET (web_view));
+	gtk_widget_show_all (main_window);
+	gtk_main ();
+
+	/* done */
+	return 0;
+}

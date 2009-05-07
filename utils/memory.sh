6f4e8ef293502acc091bb24f4b6739fa99bb152e
Brendan Neva <bneva1@my.bcit.ca>
2009-05-07 21:14:56 +0000

    added shellscript that prints a graph in the terminal

--- utils/memory.sh
+++ utils/memory.sh
@@ -1,1 +1,17 @@
-ps -eo vsz,cmd | grep spry | cut -f 1 -d \ 
+while true
+do    
+	memory=`ps -o vsz= -C spry`
+	if [ -n "$memory" ]
+	then
+		mem2="${memory} "
+		memory=`echo $memory / 25000 | bc`
+		i=0
+		while [ $i -lt $memory ]
+		do
+			mem2="${mem2}="
+			i=`echo $i + 1 | bc`
+		done
+		echo $mem2
+		i=0
+	fi
+done

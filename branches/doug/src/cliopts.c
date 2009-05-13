69a663f8aa5b2718356cf5968fd2a07dae2dedc5
Doug Penner <darwinsurvivor@gmail.com>
2009-05-13 20:01:52 +0000

    test code that fails for unknown reason

---
+++ branches/doug/src/cliopts.c
@@ -0,0 +1,197 @@
+/*
+ * cliopts.c
+ * Copyright (C) 2009 Doug Penner <darwinsurvivor@gmail.com>
+ *                    Brendan Neva <bneva1@my.bcit.ca>
+ *                    Steffen L. Norgren <ironix@trollop.org>
+ *                    Eddie Zhang <edisonhammer@gmail.com>
+ * 
+ * cliopts.c is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * cliopts.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "spry.h"
+#include "cliopts.h"
+
+/**
+ * parse_args:
+ * @argc: number of arguments passed in
+ * @argv: the arguments passed via the command line
+ *
+ * Initializes the SPRY_CONF struct and then parses command-line options to set
+ * the appropriate flag or setting inside the SPRY_CONF struct.
+ *
+ * Returns: %SPRY_CONF struct
+ **/
+SPRY_CONF*
+parse_args (int argc, char *argv[])
+{
+    SPRY_CONF *conf;
+    int    c, option_index = 0;
+    static struct option long_options[] =
+    {
+        {"url"                          , required_argument , 0, 'u'},
+        {"fullscreen"                   , no_argument       , 0, 'f'},
+        {"no-context-menu"              , no_argument       , 0, 'c'},
+        {"no-scrollbars"                , no_argument       , 0, 's'},
+        {"fixed-size"                   , no_argument       , 0, 'z'},
+        {"toolbar-height"               , required_argument , 0, 't'},
+        {"toolbar-hidden-height"        , required_argument , 0, 'T'},
+        {"width"                        , required_argument , 0, 'x'},
+        {"height"                       , required_argument , 0, 'y'},
+        {"help"                         , no_argument       , 0, 'h'},
+        {"version"                      , no_argument       , 0, 'v'},
+        {0, 0, 0, 0}
+    };
+
+    conf = malloc (sizeof (SPRY_CONF));
+    init_spry_conf (conf);
+
+    while (1)
+    {
+        c = getopt_long (argc, argv, "u:fcszt:T:x:y:hv", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+            case 0:
+                /* If the option set a flag, do nothing */
+                if (long_options[option_index].flag != 0)
+                    break;
+                break;
+
+            case 'u':
+                if (strstr (optarg, "http://") != NULL)
+                {
+                    conf->init_url = optarg;
+                }
+                else
+                {
+                    conf->init_url = malloc ((strlen ("http://") + strlen (optarg)) * sizeof (char) + 1);
+                    memset (conf->init_url, '\0', sizeof (conf->init_url));
+                                                      
+                    strcat (conf->init_url, "http://");
+                    strcat (conf->init_url, optarg);
+                }
+                break;
+
+            case 'f':
+                conf->mode = conf->mode | FULLSCREEN;
+                break;
+
+            case 'c':
+                conf->features = conf->features & !CONTEXT_MENU_ENABLED;
+                break;
+
+            case 's':
+                conf->features = conf->features & !SCROLLBARS_ENABLED;
+                break;
+
+            case 'z':
+                conf->features = conf->features & !RESIZE_ENABLED;
+                break;
+
+            case 'x':
+                conf->window_size[0] = atoi(optarg);
+                break;
+
+            case 'y':
+                conf->window_size[1] = atoi(optarg);
+                break;
+
+            case 't':
+                conf->toolbar_height = atoi(optarg);
+                break;
+
+            case 'T':
+                conf->toolbar_fullscreen_height = atoi(optarg);
+                break;
+
+            case 'h':
+                spry_usage (argv[0], OPTS_HELP);
+                break;
+                
+            case 'v':
+                g_print ("Spry - Gtk+ WebKit Browser Version %s\n", VERSION);
+                exit (0);
+                break;
+                
+            default:
+                spry_usage (argv[0], OPTS_ERROR);
+                break;
+        }
+    }
+
+    return conf;
+}
+
+/**
+ * init_spry_conf:
+ * @conf: pointer to the #SPRY_CONF struct
+ *
+ * Initializes the SPRY_CONF struct with default values.
+ **/
+void
+init_spry_conf (SPRY_CONF* conf)
+{
+    conf->init_url                  = "http://www.google.ca";
+    conf->features                  = SCROLLBARS_ENABLED | CONTEXT_MENU_ENABLED | RESIZE_ENABLED;
+    conf->mode                      = TOOLBAR;
+    conf->browser_status            = 0;
+    conf->window_size[0]            = DEFAULT_WIDTH;
+    conf->window_size[1]            = DEFAULT_HEIGHT;
+    conf->toolbar_height            = DEFAULT_TOOLBAR_HEIGHT;
+    conf->toolbar_fullscreen_height = DEFAULT_TOOLBAR_FULLSCREEN_HEIGHT ;
+}
+
+/**
+ * spry_usage:
+ * @command: the command to load the program (e.g. "./spry")
+ * @err: the type of error that initiated this function
+ *
+ * Prints out the command-line usage for the application as well as handling
+ * command-line option errors.
+ **/
+void
+spry_usage (char* command, int err)
+{
+    if (err == OPTS_HELP)
+    {
+        g_print ("Spry - Gtk+ WebKit Browser Version %s\n\n", VERSION);
+        g_print ("usage: spry [arguments]\n\n");
+        g_print ("Arguments:\n");
+        g_print ("  -u         or  --url                               The URL of the website to load\n");
+        g_print ("  -f         or  --fullscreen                        Enable fullscreen mode\n");
+        g_print ("  -c         or  --no-context-menu                   Disable context menu\n");
+        g_print ("  -s         or  --no-scrollbars                     Disable scrollbars\n");
+        g_print ("  -t [size]  or  --toolbar-height [size]             Set the height of the toolbar\n");
+        g_print ("  -T [size]  or  --toolbar-fullscreen-height [size]  Set the height of the fullscreen toolbar\n");
+        g_print ("  -z         or  --fixed-size                        Disable resizing\n");
+        g_print ("  -x [size]  or  --width [size]                      Set the width of the window\n");
+        g_print ("  -y [size]  or  --height [size]                     Set the height of the window\n");
+        g_print ("  -h         or  --help                              Prints out this screen\n");
+        g_print ("  -v         or  --version                           Prints out version information\n");
+    }
+    else if (err == OPTS_ERROR)
+    {
+        g_print ("Try `spry --help` for more information.\n");
+    }
+    else
+    {
+        g_print ("%s: unknown error\n", command);
+        g_print ("Try `spry --help` for more information.\n");
+    }
+    
+    exit (err);
+}
---
+++ branches/doug/src/generators.c
@@ -0,0 +1,297 @@
+/*
+ * generators.c
+ * Copyright (C) 2009 Doug Penner <darwinsurvivor@gmail.com>
+ *                    Brendan Neva <bneva1@my.bcit.ca>
+ *                    Steffen L. Norgren <ironix@trollop.org>
+ *                    Eddie Zhang <edisonhammer@gmail.com>
+ * 
+ * generators.c is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * generators.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "generators.h"
+#include "callbacks.h"
+#include "spry.h"
+
+/**
+ * generate_gui:
+ * @conf: pointer to a struct of config settings
+ *
+ * Creates a the entire gui (main window).
+ * 
+ * Returns: struct of main gtk objects.
+ **/
+SPRY_GTK_OBJECTS*
+generate_gui (SPRY_CONF* conf)
+{
+    
+    SPRY_GTK_OBJECTS* gtk_objects = malloc (sizeof (SPRY_GTK_OBJECTS));
+    
+    /* Create GTK Objects */
+    gtk_objects->main_window        = generate_main_window(conf);       /* Main Window */
+    gtk_objects->v_box              = gtk_vbox_new(FALSE, 0);           /* holds all the toolbars */
+    gtk_objects->toolbar            = generate_toolbar(conf);
+    gtk_objects->toolbar_fullscreen = generate_toolbar_fullscreen(conf);
+    gtk_objects->context_menu       = generate_context_menu (conf);
+    gtk_objects->scrolled_window    = gtk_scrolled_window_new (NULL, NULL);
+    gtk_objects->web_view           = WEBKIT_WEB_VIEW (webkit_web_view_new ());
+    
+    /* configure window */
+    gtk_window_set_icon_from_file((GtkWindow*) gtk_objects->main_window, "images/app.svg", NULL);
+    
+    /* resize elements */
+    gtk_widget_set_size_request (gtk_objects->toolbar_fullscreen, 10, conf->toolbar_fullscreen_height);
+    gtk_widget_set_size_request (gtk_objects->toolbar, 10, conf->toolbar_height);
+    
+    /* Configure scrolled window */
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (gtk_objects->scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    
+    /* Put GTK Objects inside each-other */
+    gtk_container_add   (GTK_CONTAINER (gtk_objects->main_window)       , gtk_objects->v_box                );
+    gtk_container_add   (GTK_CONTAINER (gtk_objects->scrolled_window)   , GTK_WIDGET (gtk_objects->web_view));
+    gtk_box_pack_start  ((GtkBox*)      gtk_objects->v_box              , gtk_objects->toolbar              , FALSE, FALSE, 0);
+    gtk_box_pack_start  ((GtkBox*)      gtk_objects->v_box              , gtk_objects->toolbar_fullscreen   , FALSE, FALSE, 0);
+    gtk_box_pack_start  ((GtkBox*)      gtk_objects->v_box              , gtk_objects->context_menu         , TRUE , TRUE , 0);
+    gtk_box_pack_start  ((GtkBox*)      gtk_objects->v_box              , gtk_objects->scrolled_window      , TRUE , TRUE , 0);
+    
+    /* show objects */
+    gtk_widget_grab_focus (GTK_WIDGET (gtk_objects->web_view));
+    gtk_widget_show_all (gtk_objects->main_window);
+    
+    /* return gtk_objects */
+    return gtk_objects;
+}
+
+/**
+ * generate_toolbar_fullscreen:
+ * @conf: pointer to a struct of config settings
+ *
+ * Creates the fullscreen toolbar for the top (fullscreen mode)
+ *
+ * Returns: A pointer to the toolbar.
+ **/
+GtkWidget*
+generate_toolbar_fullscreen (SPRY_CONF* conf)
+{
+    /* Declarations */
+    GtkWidget*  toolbar_fullscreen;
+    GtkWidget*  toolbar_button;
+    
+    /* Create buttons */
+    toolbar_fullscreen  = gtk_hbox_new(TRUE, 0);
+    toolbar_button      = generate_button("toolbar");
+    
+    /* Add buttons to toolbar */
+    gtk_box_pack_start ((GtkBox*) toolbar_fullscreen, toolbar_button, TRUE, TRUE, 0);
+    
+    /* Connect buttons to actions */
+    g_signal_connect (G_OBJECT (toolbar_button)     , "clicked"     , G_CALLBACK (callback_toolbar)   , conf);
+    
+    /* show buttons */
+    gtk_widget_show(toolbar_button);
+    
+    /* Return toolbar */
+    return toolbar_fullscreen;
+}
+
+/**
+ * generate_toolbar:
+ * @conf: pointer to a struct of config settings
+ *
+ * Creates the toolbar for the top (restored mode)
+ *
+ * Returns: A pointer to the toolbar.
+ **/
+GtkWidget*
+generate_toolbar (SPRY_CONF* conf)
+{
+    /* Declarations */
+    GtkWidget*  toolbar;
+    GtkWidget*  back_button;
+    GtkWidget*  forward_button;
+    GtkWidget*  home_button;
+    GtkWidget*  context_button;
+    GtkWidget*  toolbar_button;
+    GtkWidget*  fullscreen_button;
+    GtkWidget*  minimize_button;
+    GtkWidget*  close_button;
+    
+    /* Create buttons */
+    toolbar             = gtk_hbox_new(TRUE, 0);
+    back_button         = generate_button("back");
+    forward_button      = generate_button("forward");
+    home_button         = generate_button("home");
+    context_button      = generate_button("context");
+    toolbar_button      = generate_button("toolbar");
+    fullscreen_button   = generate_button("fullscreen");
+    minimize_button     = generate_button("minimize");
+    close_button        = generate_button("close");
+    
+    /* Add buttons to toolbar */
+    gtk_box_pack_start ((GtkBox*) toolbar, back_button          , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, forward_button       , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, home_button          , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, context_button       , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, toolbar_button       , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, fullscreen_button    , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, minimize_button      , TRUE, TRUE, 0);
+    gtk_box_pack_start ((GtkBox*) toolbar, close_button         , TRUE, TRUE, 0);
+    
+    /* Connect buttons to actions */
+    g_signal_connect (G_OBJECT (back_button)        , "clicked", G_CALLBACK (callback_back)         , conf);
+    g_signal_connect (G_OBJECT (forward_button)     , "clicked", G_CALLBACK (callback_forward)      , conf);
+    g_signal_connect (G_OBJECT (home_button)        , "clicked", G_CALLBACK (callback_home)         , conf);
+    g_signal_connect (G_OBJECT (context_button)     , "clicked", G_CALLBACK (callback_context)      , conf);
+    g_signal_connect (G_OBJECT (toolbar_button)     , "clicked", G_CALLBACK (callback_toolbar)      , conf);
+    g_signal_connect (G_OBJECT (fullscreen_button)  , "clicked", G_CALLBACK (callback_fullscreen)   , conf);
+    g_signal_connect (G_OBJECT (minimize_button)    , "clicked", G_CALLBACK (callback_minimize)     , conf);
+    g_signal_connect (G_OBJECT (close_button)       , "clicked", G_CALLBACK (callback_destroy)      , NULL);
+    
+    /* Show buttons */
+    gtk_widget_show (back_button);
+    gtk_widget_show (forward_button);
+    gtk_widget_show (home_button);
+    gtk_widget_show (toolbar_button);
+    gtk_widget_show (context_button);
+    gtk_widget_show (fullscreen_button);
+    gtk_widget_show (minimize_button);
+    gtk_widget_show (close_button);
+        
+    /* Return toolbar */
+    return toolbar;
+}
+
+/**
+ * generate_context_menu:
+ * @conf: pointer to a struct of config settings
+ *
+ * Creates the context menu.
+ *
+ * Returns: A pointer to the context menu.
+ **/
+GtkWidget*
+generate_context_menu (SPRY_CONF* conf)
+{
+    /* Declarations */
+    GtkWidget*  menu;
+    GtkWidget*  minimize;
+    GtkWidget*  fullscreen;
+    GtkWidget*  close;
+    GtkWidget*  back;
+    GtkWidget*  home;
+    GtkWidget*  forward;
+    GtkWidget*  toolbar;
+    GtkWidget*  context;
+    
+    /* Create buttons */
+    menu        = gtk_table_new(3, 3, TRUE);
+    minimize    = generate_button("minimize");
+    fullscreen  = generate_button("fullscreen");
+    close       = generate_button("close");
+    back        = generate_button("back");
+    home        = generate_button("home");
+    forward     = generate_button("forward");
+    toolbar     = generate_button("toolbar");
+    context     = generate_button("context");
+    
+    /* Add buttons to toolbar */
+    gtk_table_attach(GTK_TABLE (menu), minimize     , 0, 1, 0, 1, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), fullscreen   , 1, 2, 0, 1, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), close        , 2, 3, 0, 1, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), back         , 0, 1, 1, 2, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), home         , 1, 2, 1, 2, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), forward      , 2, 3, 1, 2, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), toolbar      , 1, 2, 2, 3, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE (menu), context      , 2, 3, 2, 3, GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    
+    /* Connect buttons to actions */
+    g_signal_connect (G_OBJECT (minimize)   , "clicked", G_CALLBACK (callback_minimize)     , conf);
+    g_signal_connect (G_OBJECT (fullscreen) , "clicked", G_CALLBACK (callback_fullscreen)   , conf);
+    g_signal_connect (G_OBJECT (close)      , "clicked", G_CALLBACK (callback_destroy)      , NULL);
+    g_signal_connect (G_OBJECT (back)       , "clicked", G_CALLBACK (callback_back)         , conf);
+    g_signal_connect (G_OBJECT (home)       , "clicked", G_CALLBACK (callback_home)         , conf);
+    g_signal_connect (G_OBJECT (forward)    , "clicked", G_CALLBACK (callback_forward)      , conf);
+    g_signal_connect (G_OBJECT (toolbar)    , "clicked", G_CALLBACK (callback_toolbar)      , conf);
+    g_signal_connect (G_OBJECT (context)    , "clicked", G_CALLBACK (callback_context)      , conf);
+    
+    /* Show buttons */
+    gtk_widget_show(minimize);
+    gtk_widget_show(fullscreen);
+    gtk_widget_show(close);
+    gtk_widget_show(back);
+    gtk_widget_show(home);
+    gtk_widget_show(forward);
+    gtk_widget_show(fullscreen);
+    gtk_widget_show(context);
+        
+    /* Return toolbar */
+    return menu;
+}
+
+/**
+ * generate_main_window:
+ * @conf: pointer to a struct of config settings
+ *
+ * Creates main window.
+ *
+ * Returns: A pointer to the main window.
+ **/
+GtkWidget*
+generate_main_window (SPRY_CONF* conf)
+{
+    /* Create Window */
+    GtkWidget* main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    
+    /* Resize the window */
+    gtk_window_set_default_size (GTK_WINDOW (main_window), conf->window_size[0], conf->window_size[1]);
+    
+    /* Set window name (taskbar / titlebar) */
+    gtk_widget_set_name (main_window, conf->title);
+    
+    /* Connect close signal to quit */
+    g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (callback_destroy), NULL);
+    
+    /* Return window */
+    return main_window;
+}
+
+/**
+ * generate_button:
+ * @icon_name: icon filename
+ *
+ * Creates a resizable button.
+ *
+ * Returns: FALSE.
+ */
+GtkWidget*
+generate_button(char* icon_name)
+{
+    /* declarations */
+    GtkWidget *button;
+    GtkWidget *draw;
+
+    /* create the button */
+    button = gtk_button_new();
+
+    /* create the drawin area (inside the button) */
+    draw = gtk_drawing_area_new();
+    
+    /* tell gtk to re-draw the icon when the button changes */
+    g_signal_connect(G_OBJECT(draw), "expose-event", G_CALLBACK(cb_expose), icon_name);
+    
+    /* add the drawing area to the button */
+    gtk_container_add(GTK_CONTAINER(button), draw);
+    
+    /* return the button */
+    return button;
+}

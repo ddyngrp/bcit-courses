048eaa84c965661c50f207a1174bed6bc91fd546
Steffen L. Norgren <ironix@trollop.org>
2009-05-19 00:58:44 +0000

    Testing some stuff with mouse clicking

---
+++ branches/testing/src/gui_funcs.h
@@ -0,0 +1,31 @@
+/*
+ * gui_funcs.h
+ * Copyright (C) 2009 Doug Penner <darwinsurvivor@gmail.com>
+ *                    Brendan Neva <bneva1@my.bcit.ca>
+ *                    Steffen L. Norgren <ironix@trollop.org>
+ *                    Eddie Zhang <edisonhammer@gmail.com>
+ * 
+ * gui_funcs.h is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * gui_funcs.h is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _GUI_FUNCS_H
+#define _GUI_FUNCS_H
+
+#include "config.h"
+
+#include "spry.h"
+
+void gui_apply_mode (SPRY_CONF*);
+
+#endif

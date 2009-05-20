c5aff02303ac8b63eed28022669e6ccf5c13a2fa
Steffen L. Norgren <ironix@trollop.org>
2009-05-20 05:19:05 +0000

    Corrected an SGML template error

--- trunk/mkdoc.sh
+++ trunk/mkdoc.sh
@@ -6,6 +6,6 @@ gtkdoc-mktmpl --module=SPRY
 gtkdoc-mkdb --module=SPRY --source-dir=../src --tmpl-dir=tmpl --output-format=xml --main-sgml-file=SPRY-docs.sgml
 gtkdoc-mkhtml SPRY SPRY-docs.sgml
 
-rm -rf *.txt *.xml *sgml* *.dev* xml* tmpl* *.types *.bak
+rm -rf *.txt *.xml *.dev* xml* tmpl* *.types *.bak index.sgml
 
 cd ..

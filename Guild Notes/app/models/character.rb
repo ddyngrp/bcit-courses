29c8ec724cfe26679ed11fc08e6f5496e4cd4e31
Steffen L. Norgren <ironix@trollop.org>
2009-06-07 06:46:45 +0000

    Starting from scratch

--- Guild Notes/app/models/character.rb
+++
@@ -1,4 +0,0 @@
-class Character < ActiveRecord::Base
-  belongs_to :person
-  #validates_presence_of :name
-end
--- Guild Notes/config/routes.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :people, :has_many => :characters
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-  map.root :controller => "home"
-end

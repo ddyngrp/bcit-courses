63b1c9bc3b15b8aea52c692462214eb84f41b515
Steffen L. Norgren <ironix@trollop.org>
2009-06-18 04:16:45 +0000

    Finished Lab #2

---
+++ COMP 2691/Lab 2 - BankLibrary/BankLibrary/BankUIForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BankLibrary
+{
+    partial class BankUIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accountTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.balanceTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // accountTextBox
+            // 
+            this.accountTextBox.Location = new System.Drawing.Point(76, 12);
+            this.accountTextBox.Name = "accountTextBox";
+            this.accountTextBox.Size = new System.Drawing.Size(196, 20);
+            this.accountTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Name";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(76, 38);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(76, 64);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.lastNameTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Balance";
+            // 
+            // balanceTextBox
+            // 
+            this.balanceTextBox.Location = new System.Drawing.Point(76, 90);
+            this.balanceTextBox.Name = "balanceTextBox";
+            this.balanceTextBox.Size = new System.Drawing.Size(196, 20);
+            this.balanceTextBox.TabIndex = 6;
+            // 
+            // BankUIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 125);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.balanceTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.accountTextBox);
+            this.Name = "BankUIForm";
+            this.Text = "BankGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox accountTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox balanceTextBox;
+    }
+}
+
---
+++ COMP 2691/Lab 2 - BankLibrary/BankLibrary/Record.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankLibrary
+{
+    public class Record
+    {
+        private int account;
+        private string firstName;
+        private string lastName;
+        private decimal balance;
+
+        public Record()
+            : this(0, "", "", 0.0M)
+        {
+        } // end constructor
+
+        public Record(int accountValue, string firstNameValue,
+            string lastNameValue, decimal balanceValue)
+        {
+            Account = accountValue;
+            FirstName = firstNameValue;
+            LastName = lastNameValue;
+            Balance = balanceValue;
+        } // end constructor
+        
+        // property that gets and sets Account
+        public int Account
+        {
+            get
+            {
+                return account;
+            } // end get
+            set
+            {
+                account = value;
+            } // end set
+        } // end property Account
+
+        // property that gets and sets FirstName
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            } // end get
+            set
+            {
+                firstName = value;
+            } // end set
+        } // end property FirstName
+
+        // property that gets and sets LastName
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            } // end get
+            set
+            {
+                lastName = value;
+            } // end set
+        } // end property LastName
+
+        // property that gets and sets Balance
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            } // end get
+            set
+            {
+                balance = value;
+            } // end set
+        } // end property Balance
+    }
+}

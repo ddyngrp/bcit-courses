b446779f8cc6eefcd919384c1282f8eae17a19fb
Steffen L. Norgren <ironix@trollop.org>
2009-06-24 11:39:31 +0000

    Almost finished with the open file section

---
+++ COMP 2691/Assignment 2 - ReadSequentialAccessFileForm/ReadSequentialAccessFileForm/ReadSequentialAccessFileForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ReadSequentialAccessFileForm
+{
+    partial class ReadSequentialAccessFileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // accountTextBox
+            // 
+            this.accountTextBox.Location = new System.Drawing.Point(86, 15);
+            this.accountTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(11, 110);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(11, 152);
+            this.label4.TabIndex = 5;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(86, 60);
+            this.firstNameTextBox.TabIndex = 3;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(86, 107);
+            this.lastNameTextBox.TabIndex = 4;
+            // 
+            // balanceTextBox
+            // 
+            this.balanceTextBox.Location = new System.Drawing.Point(86, 149);
+            this.balanceTextBox.TabIndex = 5;
+            // 
+            // openButton
+            // 
+            this.openButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.openButton.Location = new System.Drawing.Point(84, 184);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextButton.Location = new System.Drawing.Point(165, 184);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 1;
+            this.nextButton.Text = "Next Record";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // ReadSequentialAccessFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 219);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "ReadSequentialAccessFileForm";
+            this.Text = "Reading A Sequential File";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ReadSequentialAccessFileForm_FormClosed);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.firstNameTextBox, 0);
+            this.Controls.SetChildIndex(this.lastNameTextBox, 0);
+            this.Controls.SetChildIndex(this.balanceTextBox, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.accountTextBox, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.openButton, 0);
+            this.Controls.SetChildIndex(this.nextButton, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ COMP 2691/Assignment 2 - ReadSequentialAccessFileForm/ReadSequentialAccessFileForm/ReadSequentialAccessFileForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using BankLibrary;
+
+namespace ReadSequentialAccessFileForm
+{
+    public partial class ReadSequentialAccessFileForm : BankUIForm
+    {
+        private StreamReader fileReader; // reads data from a text file
+        private FileStream input;        // maintains connection to a file  
+
+        public ReadSequentialAccessFileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog(); // Displays an OpenFileDialog
+            string fileName;
+
+            ofd.Filter = "Data Files|*.dat";  // Hide all non .dat files
+            ofd.Title = "Select a Data File"; // Sets the dialogue title
+
+            // Creates a dialog box enabling user to open file
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = ofd.FileName; // Get specified file name
+
+                // Show error if user specified invalid file
+                if (!fileName.EndsWith(".dat"))
+                {
+                    MessageBox.Show("Invalid file type.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    return;
+                }
+            }
+            else // Exit event handler if user clicked Cancel
+            {
+                return;
+            }
+
+            // TODO: create FileStream to obtain read access to file
+            // TODO: set file from where data is read
+            
+            // Release resources
+            ofd.Dispose();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the next record
+                string inputRecord = fileReader.ReadLine();
+                string[] inputFields; // Stores each field of data
+
+                if (inputRecord != null)
+                {
+                    inputFields = null; // TODO???
+
+                    Record record = new Record(
+                        Convert.ToInt32(inputFields[0]),
+                        inputFields[1],
+                        inputFields[2],
+                        Convert.ToDecimal(inputFields[3]));
+
+                    // Copy string array values to TextBox values
+                    SetTextBoxValues(inputFields);
+                }
+                else
+                {
+                    // TODO: close StreamReader
+                    // TODO: close FileStream if no Records in file
+                    // TODO: enable Open File button
+                    // TODO: disable Next Record button
+                    // TODO: Clear All TextBox Controls
+                    // TODO: notify user if no Records in file
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error Reading from File", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReadSequentialAccessFileForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (fileReader != null)
+            {
+                fileReader.Close();
+                fileReader.Dispose();
+            }
+
+            if (input != null)
+            {
+                input.Close();
+                input.Dispose();
+            }
+        }
+    }
+}

5a363298d442f568e1e1313d4f9b92db7f29acb5
Steffen L. Norgren <ironix@trollop.org>
2009-06-30 03:26:34 +0000

    Two methods for connecting to a database

---
+++ COMP 2691/Class 05/ConnectionThingy/ConnectionThingy/ConnectionStrings.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ConnectionThingy
+{
+    partial class ConnectionStrings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.btnConfigString = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(12, 12);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 23);
+            this.btnGetData.TabIndex = 0;
+            this.btnGetData.Text = "Get Data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // btnConfigString
+            // 
+            this.btnConfigString.Location = new System.Drawing.Point(93, 12);
+            this.btnConfigString.Name = "btnConfigString";
+            this.btnConfigString.Size = new System.Drawing.Size(94, 23);
+            this.btnConfigString.TabIndex = 1;
+            this.btnConfigString.Text = "Config Connect";
+            this.btnConfigString.UseVisualStyleBackColor = true;
+            this.btnConfigString.Click += new System.EventHandler(this.btnConfigString_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 259);
+            this.Controls.Add(this.btnConfigString);
+            this.Controls.Add(this.btnGetData);
+            this.Name = "Form1";
+            this.Text = "Connection String Example";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Button btnConfigString;
+    }
+}
+
---
+++ COMP 2691/Class 05/ConnectionThingy/ConnectionThingy/ConnectionStrings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+using System.Configuration;
+
+namespace ConnectionThingy
+{
+    public partial class ConnectionStrings : Form
+    {
+        public ConnectionStrings()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            String strConn = @"Server=XENOWIN\SQLEXPRESS;"
+                             + "Provider=SQLOLEDB;"
+                             + "database=Northwind;"
+                             + "uid=ironix;"
+                             + "password=trustno1;";
+
+            // For OLEDB Provider
+            OleDbConnection conn = new OleDbConnection(strConn);
+
+            conn.Open();
+        }
+
+        private void btnConfigString_Click(object sender, EventArgs e)
+        {
+            String connString = ConfigurationManager.ConnectionStrings["XenoConnect"].ConnectionString;
+
+            // For SQL Provider
+            SqlConnection conn = new SqlConnection(connString);
+
+            conn.Open();
+        }
+    }
+}

87d378084b2bbc0f910598433c9fb101d71c8460
Steffen L. Norgren <ironix@trollop.org>
2009-07-06 14:01:28 +0000

    Almost finished Assignment #4. Add comments and look into speeding up loading directory info.

---
+++ COMP 2691/Assignment 4 - Explorer Style Interface/Explorer Style Interface/Explorer.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Explorer_Style_Interface
+{
+    partial class Explorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Explorer));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderName = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderType = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderModified = new System.Windows.Forms.ColumnHeader();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(641, 403);
+            this.splitContainer1.SplitterDistance = 224;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(224, 403);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder.ico");
+            this.imageList1.Images.SetKeyName(1, "document.ico");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderType,
+            this.columnHeaderModified});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(413, 403);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Name";
+            // 
+            // columnHeaderType
+            // 
+            this.columnHeaderType.Text = "Type";
+            // 
+            // columnHeaderModified
+            // 
+            this.columnHeaderModified.Text = "Last Modified";
+            // 
+            // Explorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 403);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Explorer";
+            this.Text = "Windows Explorer Style Interface";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderType;
+        private System.Windows.Forms.ColumnHeader columnHeaderModified;
+
+
+    }
+}
+
---
+++ COMP 2691/Assignment 4 - Explorer Style Interface/Explorer Style Interface/Explorer.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32; // Give access to the registry
+
+namespace Explorer_Style_Interface
+{
+    public partial class Explorer : Form
+    {
+        public Explorer()
+        {
+            InitializeComponent();
+            InitDirectoryTree(@"C:\");
+        }
+
+        private void InitDirectoryTree(String rootPath)
+        {
+            // Don't draw the TreeView until finished
+            treeView1.BeginUpdate();
+
+            // Create and add the root node
+            TreeNode rootNode = new TreeNode(rootPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the root node's folders
+            PopulateNode(rootNode);
+            treeView1.Nodes[0].Expand();
+
+            // Populate the ListView with the root node's files
+            PopulateList(rootNode);
+
+            // Update the TreeView
+            treeView1.EndUpdate();
+        }
+
+        private void PopulateNode(TreeNode dirNode)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirNode.FullPath);
+
+            // Avoid access errors
+            try
+            {
+                foreach (DirectoryInfo dirItem in dirInfo.GetDirectories())
+                {
+                    // Don't display hidden directories
+                    if ((dirItem.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                    {
+                        TreeNode newNode = new TreeNode(dirItem.Name);
+
+                        dirNode.Nodes.Add(newNode);
+
+                        // To simplify things, collapse any expanded subnodes
+                        if (dirNode.IsExpanded)
+                        {
+                            dirNode.Collapse();
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void PopulateList(TreeNode node)
+        {
+            // Don't draw the listView until finished
+            listView1.BeginUpdate();
+            listView1.Items.Clear();
+
+            DirectoryInfo dirInfo = new DirectoryInfo(node.FullPath);
+
+            // First populate the ListView with subdirectories
+            foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+            {
+                // Don't process hidden directories
+                if ((dir.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                {
+                    ListViewItem listFileItem = new ListViewItem();
+                    ListViewItem.ListViewSubItem fileType = new ListViewItem.ListViewSubItem();
+                    ListViewItem.ListViewSubItem fileModified = new ListViewItem.ListViewSubItem();
+
+                    listFileItem.Text = dir.Name;
+
+                    fileType.Text = "Directory";
+                    fileModified.Text = dir.LastWriteTime.ToString();
+
+                    listFileItem.SubItems.Add(fileType);
+                    listFileItem.SubItems.Add(fileModified);
+                    listFileItem.ImageIndex = 0;
+                    listView1.Items.Add(listFileItem);
+                }
+            }
+
+            // Populate the ListView with files
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                // Don't proces hidden files
+                if ((file.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                {
+                    ListViewItem listFileItem = new ListViewItem();
+                    ListViewItem.ListViewSubItem fileType = new ListViewItem.ListViewSubItem();
+                    ListViewItem.ListViewSubItem fileModified = new ListViewItem.ListViewSubItem();
+
+                    listFileItem.Text = file.Name;
+
+                    fileType.Text = GetFileType(file.Extension);
+                    fileModified.Text = file.LastWriteTime.ToString();
+
+                    listFileItem.SubItems.Add(fileType);
+                    listFileItem.SubItems.Add(fileModified);
+                    listFileItem.ImageIndex = 1;
+                    listView1.Items.Add(listFileItem);
+                }
+            }
+
+            if (dirInfo.GetFiles().Length > 0)
+            {
+                // Resize the columns based on the contents
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+            else
+            {
+                // Resize the ListView columns based on the header size
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+
+            // Update the listView
+            listView1.EndUpdate();
+        }
+
+        private String GetFileType(String extension)
+        {
+            // Create a registry key object to represent the HKEY_CLASSES_ROOT registry section
+            RegistryKey rkRoot = Registry.ClassesRoot;
+
+            // Attempt to retrieve the registry key for the file type
+            RegistryKey rkFileType = rkRoot.OpenSubKey(extension);
+
+            try
+            {
+                // Retrieve the file type key
+                String fileType = (String)rkFileType.GetValue(null);
+
+                // Open the file type description key
+                RegistryKey rkApp = rkRoot.OpenSubKey(fileType);
+
+                // Retrieve the file type description value
+                String fileApp = (String)rkApp.GetValue(null);
+
+                // Close registries
+                rkFileType.Close();
+                rkApp.Close();
+                rkRoot.Close();
+
+                return fileApp;
+            }
+            catch (Exception)
+            {
+                return extension;
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            // Populate the subfolder nodes to a depth of 1
+            foreach (TreeNode node in e.Node.Nodes)
+            {
+                node.Nodes.Clear();
+                PopulateNode(node);
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            // Populate the ListView with files
+            PopulateList(e.Node);
+        }
+    }
+}

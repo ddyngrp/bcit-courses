f8e1c1bda2261e8b1e0e667929cf30468c3bbed8
Steffen L. Norgren <ironix@trollop.org>
2009-07-09 03:00:18 +0000

    Finished lab 3

---
+++ COMP 2691/Class 07/Lab 3 - Thread Pool/Lab 3 - Thread Pool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab_3___Thread_Pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.Read();
+        }
+
+        static void ShowMyText(object state)
+        {
+            string myText = (string)state;
+            Console.WriteLine("Thread: {0} - {1}", Thread.CurrentThread.ManagedThreadId, myText);
+        }
+    }
+}

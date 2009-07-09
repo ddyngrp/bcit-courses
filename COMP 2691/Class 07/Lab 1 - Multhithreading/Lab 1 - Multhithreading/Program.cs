43b5de773028da18d40a089a77f56d755f63d987
Steffen L. Norgren <ironix@trollop.org>
2009-07-09 02:34:41 +0000

    Lab 1 complete

---
+++ COMP 2691/Class 07/Lab 1 - Multhithreading/Lab 1 - Multhithreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab_1___Multhithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+            first.Start();
+            second.Start();
+            first.Join();
+            second.Join();
+            Console.Read();
+        }
+
+        static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread: {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}

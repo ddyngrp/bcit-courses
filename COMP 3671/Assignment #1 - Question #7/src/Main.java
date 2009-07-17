648d0024339c7061dd6be173a2360df50120522f
Steffen L. Norgren <ironix@trollop.org>
2009-07-17 17:38:24 +0000

    Initial Commit

---
+++ COMP 3671/Assignment #1 - Question #7/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Assignment #1 - Question #7
+ * @author Steffen L. Norgren - A00683006
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+}

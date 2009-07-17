578af7dcd82d13132af47cf99d53f19d29d1015d
Steffen L. Norgren <ironix@trollop.org>
2009-07-17 20:05:27 +0000

    Final Assignment

--- COMP 3671/Assignment #1 - Question #7/src/BubbleSort.java
+++ COMP 3671/Assignment #1 - Question #7/src/BubbleSort.java
@@ -1,4 +1,26 @@
 
 public class BubbleSort {
-
+	
+	public BubbleSort() {
+		
+	}
+	
+	public long sort(int[] input) {
+		int[] output = input.clone();
+		long basicOps = 0;
+		
+		for (int i = 0; i <= output.length - 2; ++i) {
+			for (int j = 0; j <= output.length - 2 - i; ++j) {
+				if (output[j + 1] < output[j]) {
+					int tmpSwap = output[j];
+					output[j] = output[j + 1];
+					output[j + 1] = tmpSwap;
+					basicOps++;
+				}
+			}
+		}
+		
+		return basicOps;
+	}
+	
 }
--- COMP 3671/Assignment #1 - Question #7/src/SelectionSort.java
+++ COMP 3671/Assignment #1 - Question #7/src/SelectionSort.java
@@ -1,4 +1,29 @@
 
 public class SelectionSort {
 
+	public SelectionSort() {
+		
+	}
+	
+	public long sort(int[] input) {
+		int[] output = input.clone();
+		long basicOps = 0;
+		
+		for (int i = 0; i <= output.length - 2; ++i) {
+			int min = i;
+			
+			for (int j = i + 1; j <= output.length - 1; ++j) {
+				if (output[j] < output[min]) {
+					min = j;
+					basicOps++;
+				}
+			}
+			basicOps++;
+			int tmpSwap = output[i];
+			output[i] = output[min];
+			output[min] = tmpSwap;
+		}
+		
+		return basicOps;
+	}
 }

e499db82b9f745974c4ee5b13f74992eccc46824
Steffen L. Norgren <ironix@trollop.org>
2009-12-08 11:38:35 +0000

    This should be my final product.

---
+++ COMP 7615 - Cryptography/Assignments/Final Project/MacGPGTool/MacGPGToolAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  MacGPGToolAppDelegate.m
+//  MacGPGTool
+//
+//  Created by Steffen L. Norgren on 09-12-08.
+//  Copyright 2009 Esurient Systems Inc.. All rights reserved.
+//
+
+#import "MacGPGToolAppDelegate.h"
+
+@implementation MacGPGToolAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
---
+++ COMP 7615 - Cryptography/Assignments/Final Project/MacGPGTool/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacGPGTool
+//
+//  Created by Steffen L. Norgren on 09-12-08.
+//  Copyright 2009 Esurient Systems Inc.. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}

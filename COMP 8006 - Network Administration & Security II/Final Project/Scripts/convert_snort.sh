6f2b5ae46d1740190b19e26c7376f86f54e64c14
Steffen L. Norgren <ironix@trollop.org>
2010-04-17 13:38:18 +0000

    Updated scripts

--- COMP 8006 - Network Administration & Security II/Final Project/Scripts/convert_snort.sh
+++ COMP 8006 - Network Administration & Security II/Final Project/Scripts/convert_snort.sh
@@ -2,10 +2,10 @@
 # Converts binary snort logs in all subdirectories to ASCII dumps
 
 # First delete the old converted files
-find ../ -name snort.log.*.txt -exec rm -rf {} \;
+find ../data -name snort.log.*.txt -exec rm -rf {} \;
 
 # Convert all the binary files to text
-find ../ -name snort.log.*[0-9] | while read line
+find ../data -name snort.log.*[0-9] | while read line
 do
 	tcpdump -r "$line" -tt > "$line.txt" &
 done
--- COMP 8006 - Network Administration & Security II/Final Project/Scripts/convert_tcpdump.sh
+++ COMP 8006 - Network Administration & Security II/Final Project/Scripts/convert_tcpdump.sh
@@ -2,10 +2,10 @@
 # Converts binary tcpdump logs in all subdirectories to ASCII dumps
 
 # First delete the old converted files
-find ../ -name tcpdump*.txt -exec rm -rf {} \;
+find ../data -name tcpdump*.txt -exec rm -rf {} \;
 
 # Convert all the binary files to text
-find ../ -name tcpdump* | while read line
+find ../data -name tcpdump* | while read line
 do
 	tcpdump -r "$line" -tt > "$line.txt" &
 done

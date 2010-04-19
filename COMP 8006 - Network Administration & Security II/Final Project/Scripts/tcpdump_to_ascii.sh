566b212a57f960889f3106896508e13cee398b91
Steffen L. Norgren <ironix@trollop.org>
2010-04-19 05:38:03 +0000

    Final script

--- COMP 8006 - Network Administration & Security II/Final Project/Scripts/tcpdump_to_ascii.sh
+++ COMP 8006 - Network Administration & Security II/Final Project/Scripts/tcpdump_to_ascii.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Converts binary logs in all subdirectories to ASCII dumps
 
-# First delete the old converted files
-find ../data_indexing -name '*-index' -exec rm {} \;
-
 # Convert all the binary files to text
 find ../data_indexing -name 'snort.log.*[0-9]' -o -name 'tcpdump*' | while read line
 do
-	tcpdump -r "$line" -nn -XX -tt > "$line-index" &
+	tcpdump -r "$line" -nn -X -tt 'ip' > "$line-index" && rm "$line" && mv "$line-index" "$line" &
 done

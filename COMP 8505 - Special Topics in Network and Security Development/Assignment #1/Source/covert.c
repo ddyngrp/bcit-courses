589debecb8dba122a06fb7d9ddf13742acd76d2e
Steffen L. Norgren <ironix@trollop.org>
2010-05-17 21:07:42 +0000

    Added some GNU type stuff

---
+++ COMP 8505 - Special Topics in Network and Security Development/Assignment #1/Source/covert.c
@@ -0,0 +1,42 @@
+/*-----------------------------------------------------------------------------
+ * covert.c
+ * Copyright (C) 2010 Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * covert.c is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * covert.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *----------------------------------------------------------------------------*/
+
+#include "covert.h"
+
+/*-----------------------------------------------------------------------------
+ * FUNCTION:    
+ * 
+ * DATE:        May 17, 2010
+ * 
+ * REVISIONS:   
+ * 
+ * DESIGNER:    Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * PROGRAMMER:  Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * INTERFACE:   
+ * 
+ * RETURNS:     
+ * 
+ * NOTES: 
+ *
+ *----------------------------------------------------------------------------*/
+
+int main (int argc, const char * argv[]) {
+    return 0;
+}
---
+++ COMP 8505 - Special Topics in Network and Security Development/Assignment #1/Source/covert.h
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------
+ * covert.h
+ * Copyright (C) 2010 Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * covert.h is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * covert.h is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *----------------------------------------------------------------------------*/
+
+#include <stdio.h>

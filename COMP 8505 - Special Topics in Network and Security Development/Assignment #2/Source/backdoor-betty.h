5c5c67e4deea79fdf0955a3c4edfa4a06d26db58
Steffen L. Norgren <ironix@trollop.org>
2010-06-08 05:34:01 +0000

    Started work finally!

---
+++ COMP 8505 - Special Topics in Network and Security Development/Assignment #2/Source/backdoor-betty.h
@@ -0,0 +1,22 @@
+/*-----------------------------------------------------------------------------
+ * backdoor-betty.h - Linux backdoor server (runs on comprimised system)
+ * Copyright (C) 2010 Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *----------------------------------------------------------------------------*/
+
+#ifndef BACKDOOR_BETTY_H
+#define BACKDOOR_BETTY_H
+
+#endif
---
+++ COMP 8505 - Special Topics in Network and Security Development/Assignment #2/Source/backdoor-carl.c
@@ -0,0 +1,45 @@
+/*-----------------------------------------------------------------------------
+ * backdoor-carl.h - Linux backdoor client (remote server control)
+ * Copyright (C) 2010 Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *----------------------------------------------------------------------------*/
+
+#include "backdoor-carl.h"
+
+/*-----------------------------------------------------------------------------
+ * FUNCTION:    main 
+ * 
+ * DATE:        June 4, 2010
+ * 
+ * REVISIONS:   
+ * 
+ * DESIGNER:    Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * PROGRAMMER:  Steffen L. Norgren <ironix@trollop.org>
+ * 
+ * INTERFACE:   int main(int argc, char **argv)
+ *                  argc - argument count
+ *                  argv - array of arguments
+ * 
+ * RETURNS:     Result on success or failure.
+ *
+ * NOTES: Main entry point into the program. Parses command-line arguments and
+ *        configures the server.
+ *
+ *----------------------------------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+	return 0;
+}

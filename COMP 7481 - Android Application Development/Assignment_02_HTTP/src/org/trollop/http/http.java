4ea0c7314df17052111e84ae573ff3c7785a2441
Steffen L. Norgren <ironix@trollop.org>
2011-02-07 10:31:56 +0000

    Prepared skeleton projects for remaining assignments.

---
+++ COMP 7481 - Android Application Development/Assignment_02_HTTP/src/org/trollop/http/http.java
@@ -0,0 +1,13 @@
+package org.trollop.http;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class http extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ COMP 7481 - Android Application Development/Assignment_03_2D-Plotting/src/org/trollop/plot/plot.java
@@ -0,0 +1,13 @@
+package org.trollop.plot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class plot extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

467ea3c35bb8b1239a8188c9a59899230113e388
Steffen L. Norgren <ironix@trollop.org>
2011-02-10 16:16:32 +0000

    Finished!

--- COMP 7481 - Android Application Development/Assignment_02_Server/app/controllers/application_controller.rb
+++ COMP 7481 - Android Application Development/Assignment_02_Server/app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
+  helper :all
   protect_from_forgery
+  helper_method :detect_browser
+  
+  private
+  MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm", "hiptop", "avantgo", 
+    "plucker", "xiino", "blazer", "elaine", "windows ce; ppc;", "windows ce; smartphone;",
+    "windows ce; iemobile", "up.browser", "up.link", "mmp", "symbian", "smartphone", "midp",
+    "wap", "vodafone", "o2", "pocket", "kindle", "mobile", "pda", "psp", "treo"]
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      #true
+      username == "android" && password == "android"
+    end
+  end
+
+  def detect_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      return "Browsing from a mobile device." if agent.match(m)
+    end
+    return "Browsing from a desktop machine."
+  end
 end
--- COMP 7481 - Android Application Development/Assignment_02_Server/app/controllers/home_controller.rb
+++ COMP 7481 - Android Application Development/Assignment_02_Server/app/controllers/home_controller.rb
@@ -3,11 +3,4 @@ class HomeController < ApplicationController
   
   def index
   end
-  
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      true
-      #username == "android" && password == "android"
-    end
-  end
 end

8af725646ff0e833f8667546dc8fe8ec06a420ef
Steffen L. Norgren <ironix@trollop.org>
2011-04-13 23:14:12 +0000

    Deleting reference code.

--- COMP 7481 - Android Application Development/AndroidXml/src/org/developerworks/android/FeedParser.java
+++
@@ -1,6 +0,0 @@
-package org.developerworks.android;
-import java.util.List;
-
-public interface FeedParser {
-	List<Message> parse();
-}
--- COMP 7481 - Android Application Development/AndroidXml/src/org/developerworks/android/ParserType.java
+++
@@ -1,8 +0,0 @@
-package org.developerworks.android;
-/**
- * 
- */
-
-public enum ParserType{
-	SAX, DOM, ANDROID_SAX, XML_PULL;
-}

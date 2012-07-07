f456e9bf06a0189ec25e2eb19da518c934e42c1d
Steffen L. Norgren <ironix@nein.ca>
2012-07-06 21:29:34 -0700

    Starting work on a proper command-line client. Will subsequently work on an OS X GUI version.

---
+++ COMP 8045 - Major Project/source/server/server_control.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <paths.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ioccom.h>
+
+void execute();
+int main(int, char *);
